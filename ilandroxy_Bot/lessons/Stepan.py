233e13f73db95f0ee076259eb15b554c8cc4fd21
Ilya Andrianov <ilandroxxy@gmail.com>
2022-10-14 20:01:22 +0700

    14.10.2022

--- ilandroxy_Bot/lessons/Stepan.py
+++ ilandroxy_Bot/lessons/Stepan.py
@@ -1,44 +1,84 @@
-# Домашка
-#Задача №1
+#Задание №1
 '''
-x = int(input('x: '))
-if x % 2 != 0:
-    print(f'YES')
-elif x % 2 == 0 and x >= 2 and x <= 5:  # 2 <= x <= 5
-    print(f'NO')
-elif x % 2 == 0 and x >= 6 and x <= 20:
-    print(f'YES')
-elif x % 2 == 0 and x > 20:
-    print(f'NO')
+M = []
+for i in range(10000):
+    n = int(input('n: '))
+    if n % 2 == 0:
+        M.append(n)
+        print(M)
+    else:
+        print('Число нечётное!!')
 '''
 
-#Задача №2
 '''
-x1 = int(input('x1: '))
-x2 = int(input('x2: '))
-x3 = int(input('x3: '))
-M = [x1, x2, x3]
-M.sort()
-print(M[1])
+M = []
+n = int(input('n: '))
+for x in range(1, n+1):
+    if x % 2 == 0:
+        M.append(x)
+print(M)
 '''
 
-"""
-x1 = int(input('x1: '))
-x2 = int(input('x2: '))
-x3 = int(input('x3: '))
-M = [x1, x2, x3]
-#    0   1   2
-M.sort()
-M.reverse()
-print(M[1])
 
+#Задание №2
+'''
+L = []
+M = []
+S = []
+for m in range(3):
+    n1 = int(input('n1: '))
+    M.append(n1)
+print(M)
+for l in range(3):
+    n2 = int(input('n2: '))
+    L.append(n2)
+print(L)
+for i in range(3):
+    S.append(M[i] + L[i])
+print(S)
+'''
 
-print(sum(M) - (min(M) + max(M)))
-"""
+'''
+M = [int(i) for i in input().split()]
+L = [int(i) for i in input().split()]
+# print(L)
+# print(M)
+while len(M) != len(L):
+    if len(M) < len(L):
+        M.append(0)
+    else:
+        L.append(0)
+# print(L)
+# print(M)
 
+S = []
+for i in range(len(M)):
+    S.append(M[i] + L[i])
+print(S)
+'''
 
+'''
+ip = '192.45.90.7'
+M = ip.split('.')  # метод split() разбивает строку на список строк
+print(M)
+for i in range(0, len(M)):
+    M[i] = int(M[i])
+print(M)
 
-#Задача №3
+#Задание №3
+
+x = str(input('Введите предложение: '))
+M = x.split()  # split() без аргумента - разделяет по пробелам
+print(M)
+
+maxi = 0
+for x in M:
+    if maxi < len(x):
+        maxi = len(x)
+print(maxi)
+'''
+
+#Задание №4
 '''
 x1 = int(input('x1: '))
 y1 = int(input('y1: '))
@@ -46,222 +86,104 @@ x2 = int(input('x2: '))
 y2 = int(input('y2: '))
 if x1 == x2 and y1 == y2:
     print('Фигура должна сделать ход!')
-elif x1+1 == x2 or y1+2 == y2:
+elif x1+1 == x2 or y1 == y2:
     print('YES')
-elif x1+1 == x2 or y1-2 == y2:
+elif x1+1 == x2 or y1+1 == y2:
     print('YES')
-elif x1-1 == x2 or y1+2 == y2:
+elif x1-1 == x2 or y1 == y2:
     print('YES')
-elif x1-1 == x2 or y1-2 == y2:
+elif x1-1 == x2 or y1+1 == y2:
     print('YES')
-elif x1+2 == x2 or y1+1 == y2:
+elif x1 == x2 or y1+1 == y2:
     print('YES')
-elif x1-2 == x2 or y1+1 == y2:
+elif x1 == x2 or y1-1 == y2:
     print('YES')
-elif x1+2 == x2 or y1-1 == y2:
+elif x1-1 == x2 or y1-1 == y2:
     print('YES')
-elif x1-2 == x2 or y1-1 == y2:
+elif x1+1 == x2 or y1-1 == y2:
     print('YES')
 else:
     print('NO')
 '''
 
+# Теория: Правила логики в Питоне
+# отрицание (инверсия)  ¬y   <---->   (not(y))
+# конъюнкция   x ∧ y  <---->   x and y
+# дизъюнкция  x ∨ y  <---->   x or y
+# тождество  x ≡ z  <---->   x == z
+# Импликация z → w  <---->  z <= w    (¬z ∨ w)
 
-# Циклы
 
-# Ключевые слова: while, for, range, len,  break, continue, flag
+# region Тип 2 № 19051
+# Миша заполнял таблицу истинности функции (x ∧ ¬y) ∨ (x ≡ z) ∨ ¬w, но успел
+# заполнить лишь фрагмент из трёх различных её строк, даже не указав,
+# какому столбцу таблицы соответствует каждая из переменных w, x, y, z.
+"""
+print('x y z w')
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            for w in range(2):
+                F = (x and (not(y))) or (x == z) or (not(w))
+                if F == False:
+                    print(x, y, z, w, F)
+"""
+# Ответ: xwzy
+# endregion Тип 2 № 19051
 
+# region Тип 2 № 15124
+# Логическая функция F задаётся выражением (x ≡ y ) ∨ ((y ∨ z) → x).
+# Дан частично заполненный фрагмент, содержащий неповторяющиеся строки таблицы истинности функции F.
+# Определите, какому столбцу таблицы истинности соответствует каждая из переменных x, y, z.
+print('x y z')
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            F = (x == y) or ((y or z) <= x)
+            if F == False:
+                print(x, y, z, F)
+# Ответ: xzy
+# endregion Тип 2 № 15124
 
-# range
+# Списки (массивы) - list()
 '''
-# повтори n раз
-for i in range(10):  # [0,10)  - от 0 это стандартное значение start при одном аргументе в range(STOP)
-    print(i, end=' ')
-print()
+M = [1, 2, 3]
+print(M, type(M))
 
-# пройди от a до b
-for i in range(1, 10):  # [1,10) range(START, STOP)
-    print(i, end=' ')
-print()
+print(len(M))  # функция len() выводит длину списка (то есть кол-во элементов в нем)
 
-# пройди от a до b c шагом 2
-for i in range(0, 10, 2):  # [1,10) range(START, STOP, STEP) шаг
-    print(i, end=' ')
-print()
+A = []  # пустой список
 
-
-for i in range(100, 0, -1):  # [1,10) range(START, STOP, STEP) шаг
-    print(i, end=' ')
-print()
-'''
-
-# len() - функция которая возвращает длину списка (строки) - то есть кол-во элементов в нем
-
-# Сценарии использования цикла фор со списками (строками)
-'''
-M = [4, 5, 6, 7, 8]
+M = [3, 4, 5, 6, 7]
 #    0  1  2  3  4
+# Упорядоченные данные - индексируемые (у каждого элемента есть свой порядковый номер - индекс)
+# Индексация начинается с нуля (последний элемент списка имеет индекс -1)
+# Элементы списка - можно менять
 
+print(M[2])
 
-# по факту мы пробегаем список через индексы его элементов
-for i in range(0, len(M)):  # [0, 5)
+for i in range(0, len(M)):
     print(M[i], end=" ")
 print()
 
-# Просто пробегаем список прям по элементам
-for x in M:
-    print(x, end=" ")
+for i in range(len(M)-1, -1, -1):
+    print(M[i], end=" ")
 print()
 
 
+M = [3, 4, 5, 6, 7]
 for i in range(0, len(M)):
     M[i] = M[i] ** 2
 print(M)
-print(*M)
 '''
 
+# НА след. уроке: срезы список, функции списков, методы списков, системы счисления, 14 номер
 
-# Ввод списка с клавиатуры:
-
-'''
-n = int(input("Кол-во элеменитов в списке: "))
+x = 8**2020 + 4**2017 + 26 - 1
 M = []
-for i in range(n):
-    M.append(int(input(f"Добавьте {i+1} элемент: ")))
-print(M)
-'''
-
-'''
-s = '1234'
-for x in s:
-    print(x * 4)
-
-for x in s:
-    print(int(x) * 4)
-
-M = [x for x in s]
-print(M)
-
-M = [int(x) for x in s]
-print(M)
-
-# M = [int(x) for x in input('Введите числовую строку: ')]
-# print(M)
-
-
-M = [int(x) for x in input().split() if int(x) % 2 == 0]
-print(M)
-'''
-
-# while - цикл с условием, он выполняется, только лишь если его условие истинно
-'''
-for i in range(0, 10+1, 2):  # [1,10) range(START, STOP, STEP)
-    print(i, end=' ')
-print()
-
-i = 0
-while i < 10 or i == 10:
-    print(i, end=' ')
-    i += 2
-print()
-
-# Сумма цифр числа
-x = 12345
-summ = 0
 while x > 0:
-    summ += x % 10
-    x //= 10
-print(summ)
-
-# Факториал числа
-x = int(input('x: '))
-temp = x
-f = 1
-while x > 0:
-    f *= x  # f = f * x
-    x -= 1  # x = x - 1
-print(f'Факториал числа {temp} = {f}')
-
-# Факториал числа через range
-x = int(input('x: '))
-f = 1
-for i in range(1, x+1):
-    f *= i
-print(f'Факториал числа {x} = {f}')
-'''
-
-
-# Бесконечный цикл
-'''
-k = 0
-while True:
-    k += 1
-    print(k)
-'''
-
-
-'''  
-import random
-
-count = 0
-password = 'qwerty'
-while True:
-    pas = input('Введите пароль для входа: ')
-    if pas == password:
-        print("Welcome")
-        break
-    print('Пароль неверный, попробуйте снова.')
-    count += 1
-    if count == 3:
-        a = random.randint(1, 100)
-        b = random.randint(1, 100)
-        print(f'Пройдите проверку на робота, решив пример:\n'
-              f'{a} + {b} =')
-        x = int(input())
-        if x == a+b:
-            count = 0
-            print('Проверка пройдена')
-            continue
-        else:
-            print('БАН')
-            break
-'''
-
-
-# break - прерывает выполнение цикла, выходит их него
-# continue - прерывает итерацию цикл
-'''
-for i in range(1, 20):
-    if i == 15:
-        break
-    if i % 2 != 0:
-        continue
-    print(i)
-'''
-
-summ = 0
-maxi = 0
-mini = 0
-count = 0
-flag = True
-
-for i in range(1234, 12394+1):
-    if i % 2 == 0 and i % 4 == 0 and i % 7 != 0 and i % 17 != 0:
-        if flag == True:
-            mini = i
-            flag = False
-        count += 1
-        summ += i
-        maxi = i
-print(f'summ = {summ}, maxi = {maxi}, mini = {mini}, count = {count}')
-
-
-
-
-
-
-
-
-
+    M.append(x % 2)
+    x //= 2
+print(M.count(1))
 
 
