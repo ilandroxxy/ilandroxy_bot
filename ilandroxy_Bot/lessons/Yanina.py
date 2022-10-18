948f0ea11bda28f3d6f8c37b89f101525442a7f0
Ilya Andrianov <ilandroxxy@gmail.com>
2022-10-18 11:28:40 +0700

    18.10.2022

--- ilandroxy_Bot/lessons/Yanina.py
+++ ilandroxy_Bot/lessons/Yanina.py
@@ -1,64 +1,65 @@
 
-# Проверка на аннаграмму
-"""
-s1 = input()
-s2 = input()
+# import turtle  # стандартное подключение библиотеки в Python проект. Везде таскаем с собой turtle.
+# turtle.forward(1000)
+# turtle.done()
 
-base1 = {}
-for i in s1:
-    base1[i] = base1.get(i, 0) + 1
-print(base1)
+# import turtle as t  # переименуем turtle. в t. (или любое другое имя)
+# t.forward(1000)
+# t.done()
 
-base2 = {}
-for i in s2:
-    base2[i] = base2.get(i, 0) + 1
-print(base2)
+# from turtle import forward, done  # подключаем только определенные функции из библиотеки
+# forward(1000)
+# done()
 
-if base1 == base2:
-    print('YES')
-else:
-    print('NO')
-"""
-
-# Проверяем и выводим синонимы из словаря
-"""
-n = int(input('n: '))
-
-base = {}
-for i in range(n):
-    M = [i for i in input().split()]
-    base[M[0]] = M[1]
-print(base)
-
-s = input('s: ')
-for key in base:
-    if s == key:
-        print(base[s])
-    if s == base[s]:
-        print(s)
-"""
+# from turtle import *  # Подключаем все функции из библиотеки (не нужно использовать turtle. )
+# forward(1000)
+# pu()
+# done()
 
 
-# Страны и города
+'''
+import turtle as t
 
-n = int(input('Введите кол-во стран: '))
+for i in range(4):
+    t.forward(100)
+    t.left(90)
 
-base = {}
-for i in range(n):
-    M = [i for i in input("Введите страну и ее города: ").split()]
-    base[M[0]] = M[1:]
-print(base)
+t.color('red')
+for i in range(2):
+    t.forward(400)
+    t.left(90)
+    t.forward(200)
+    t.left(90)
 
-# for i in base["Россия"]:
-#     print(i)
+t.done()
 
-m = int(input("Введите кол-во городов: "))
+'''
 
-for i in range(m):
-    town = input('Введите город: ')
-    for key in base:
-        if town in base[key]:
-            print(key)
+# Тип 6 № 47392
+# Черепахе был дан для исполнения следующий алгоритм:
+#
+# Повтори 6 [Вперёд 10 Направо 60]
+#
+# Определите, сколько точек с целочисленными координатами будут находиться внутри области,
+# ограниченной линией, заданной данным алгоритмом. Точки на линии учитывать следует.
 
+# Eё голова направлена вдоль положительного направления оси ординат, хвост опущен.
 
+import turtle as t  # 1
+t.left(90)   # 2
 
+t.speed(10)  # 3
+
+l = 30  # 4
+
+for _ in range(6):
+    t.forward(10 * l)  # 5
+    t.right(60)
+
+t.color('red')  # 6
+t.pu()  # 7
+for x in range(0, 20):
+    for y in range(-5, 20):
+        t.goto(x * l, y * l)  # 8
+        t.dot(3)  # 9
+t.done()  # 10
