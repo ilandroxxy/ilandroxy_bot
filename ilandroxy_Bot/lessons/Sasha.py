290db6d226d67a6ab5d6afc299d4940cd2e34f58
Ilya Andrianov <ilandroxxy@gmail.com>
2022-11-24 00:03:59 +0700

    24.11.2022

--- ilandroxy_Bot/lessons/Sasha.py
+++ ilandroxy_Bot/lessons/Sasha.py
@@ -8,112 +8,97 @@
 # region Урок: ************************************************************
 
 '''
-x = 5  # 5!
-r = 1
-while x > 0:
-    r *= x
-    x -= 1
-print('Факториал числа: ', r)
+class Car(object):
+    brand = 'Mazda'
+    max_speed = 100
+    color = 'black'
+
+    def __init__(self, b, ms):  # декоратор
+        self.brand = b
+        self.max_speed = ms
+
+    def upgrade(self):
+        self.max_speed += 25
 
 
+class Truck(Car):
+    kol_pr = 2
+    max_mass = 20
 
-x = 10  # 5!
-r = 1
-while x > 0:
-    r *= x
-    x -= 1
-print('Факториал числа: ', r)
+    def AddGruz(self, k, mas):
+        self.kol_pr = k
+        self.max_mass = mas
 
+obj = Truck('Nissan', 120)
+obj.AddGruz(10, 20)
 
-
-x = 6  # 5!
-r = 1
-while x > 0:
-    r *= x
-    x -= 1
-print('Факториал числа: ', r)
-'''
-
-
-# Написали свою функцию факториала
-'''
-def MyFactorial(x):  # x, r - Локальные переменные
-    r = 1
-    while x > 0:
-        r *= x
-        x -= 1
-    return r
-
-x = MyFactorial(5)
-print(x)
-print(MyFactorial(10))
-print(MyFactorial(6))
+print(obj.brand)
+print(obj.max_speed)
+print(obj.color)
+print(obj.kol_pr)
+print(obj.max_mass)
 '''
 
 
 '''
-a = int(input('a: '))
-b = int(input('b: '))
-c = int(input('c: '))
+class apple(object):
+    type_device = ''
+    color = ''
+    model = ''
+    service_life = 0
 
-maxi = max(a, b, c)
-mini = min(a, b, c)
+    def __init__(self, dev, col, mod, lif):
+        self.type_device = dev
+        self.color = col
+        self.model = mod
+        self.service_life = lif
 
-sred = (a + b + c) - (maxi + mini)
+    def upgrade(self, dev, col, mod, lif):
+        self.type_device = dev
+        self.color = col
+        self.model = mod
+        self.service_life = lif
 
-print(maxi, sred, mini)
+class iphone(apple):
+    size = 0
+    storage = 0
+    processor = ''
+    price = 0
+
+    def descriptions_iphone(self, siz, stor, proc, pri):
+        self.size = siz
+        self.storage = stor
+        self.processor = proc
+        self.price = pri
+
+    def HowDoshiksInIphone(self, n):
+        doshik = int(input('Введите цену доширака: '))
+        print(n / doshik)
+
+
+class macbook(apple):
+    pass
+
+class airpods(apple):
+    pass
+
+iphone_Sasha = apple('phone', 'white', '8+', 1)
+print(iphone_Sasha.color)
+
+iphone_Sasha.upgrade('Phone', 'SilverEdition', '8plus', 2)
+print(iphone_Sasha.color)
+
+iphone_Ilya = iphone('Phone', 'green', '13 Pro Max', 1)
+iphone_Ilya.descriptions_iphone(6.8, 128, 'A15 Bionic', 90000)
+print(iphone_Ilya.storage)
+
+iphone_Ilya.HowDoshiksInIphone(iphone_Ilya.price)
 '''
 
-# Написали свою функцию поиска среднего из трех чисел
-'''
-def sred(x, y, z):
-    maxi = max(x, y, z)
-    mini = min(x, y, z)
-
-    sr = (x + y + z) - (maxi + mini)
-    return sr
-
-a = int(input('a: '))
-b = int(input('b: '))
-c = int(input('c: '))
-
-maxi = max(a, b, c)
-sr = sred(a, b, c)
-mini = min(a, b, c)
-
-print(f'Среднее число из тройки равно: {sr}')
-'''
-
-
-# Тип 15 № 17336
-"""
-# Для какого наименьшего целого неотрицательного числа A выражение
-# (3x + 4y ≠ 60) ∨ ((A ≥ x) ∧ (A ≥ y))
-#
-# тождественно истинно при любых целых неотрицательных x и y?
-
-def F(A, x, y):
-    return ((3*x + 4*y) != 60) or ((A >= x) and (A >= y))
-
-for A in range(0, 1000):
-    flag = True
-    for x in range(0, 100):
-        for y in range(0, 100):
-            if F(A, x, y) == False:
-                flag = False
-                break
-        if flag == False:
-            break
-    if flag == True:
-        print(A)
-        break 
-"""
-
-# Ответ: 20.
 
 
 # endregion Урок: ************************************************************
 
 
 
-# todo: на следующем уроке: Оформляем портфолио для первого нашего бота.
+# todo: на следующем уроке: Оформляем портфолио для первого нашего бота, отвечаем на вопросы по курсы или решаем задачки
