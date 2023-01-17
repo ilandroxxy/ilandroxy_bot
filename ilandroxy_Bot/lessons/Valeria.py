283de59eaf4c810e6b77c510df404664d7549a44
Илья Андрианов <ilandroxxy@gmail.com>
2023-01-17 19:13:59 +0700

    17.01.23

--- ilandroxy_Bot/lessons/Valeria.py
+++ ilandroxy_Bot/lessons/Valeria.py
@@ -208,14 +208,14 @@ for x in range(0, 37):
 # тождественно истинна при любых натуральных значениях переменных x и y.
 
 def F(x, y):
-    return ((144 % x == 0) <= (x % y != 0)) or (x + y < 100) or (A - x > y)
+    return ((144 % x == 0) <= (x % y != 0)) or (x + y > 100) or (A - x > y)
 
 for A in range(1, 1000):
     if all(F(x, y) for x in range(1, 100) for y in range(1, 100)):
         print(A)
         break
 '''
-# Ответ: 145
+# Ответ: 97
 
 
 
