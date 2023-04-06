abeec8389c444cac88bc008d0e805e50d8df1249
ilandroxxy <ilandroxxy@gmail.com>
2023-04-06 23:30:29 +0700

    6.04.2023

--- ilandroxy_Bot/lessons/Sergey.py
+++ ilandroxy_Bot/lessons/Sergey.py
@@ -1,47 +1,62 @@
 
 # region Домашка: ******************************************************************
 
-# Тип 25 № 33770
+'''Тип 17 № 37373
+M = [int(i) for i in open('17.txt')]
+c = 0
+maxraz = -999
+for i in range(len(M) - 1):
+    for y in range(i + 1, len(M)):
+        if (max(M[i], M[y]) - min(M[i], M[y])) % 36 == 0 and (M[i] % 13 == 0 or M[y] % 13 == 0):
+                c += 1
+                if max(M[i], M[y]) - min(M[i], M[y]) > maxraz:
+                    maxraz = max(M[i], M[y]) - min(M[i], M[y])
+print(c, maxraz)
 '''
-def f(n):
-    sps = set()
-    for i in range(1, int(n**0.5) + 1):
-        if n % i == 0:
-            if i % 2 == 0:
-                sps.add(i)
-            if (n // i) % 2 == 0:
-                sps.add(n // i)
-            if len(sps) > 3:
-                return sps
-    return sps
 
-for j in range(106_000_000, 107_000_001):
-    d = f(j)
-    if len(d) == 3:
-        print(j)
+'''Тип 17 № 37337
+M = [int(i) for i in open('17.txt')]
+maxim = -9999
+c = 0
+for i in range(len(M) - 1):
+    for j in range(i + 1, len(M)):
+        if M[i] % 160 != M[j] % 160 and (M[i] % 7 == 0 or M[j] % 7 == 0):
+            c += 1
+            if maxim < M[i] + M[j]:
+                maxim = M[i] + M[j]
+print(c, maxim)
+'''
+
+'''Тип 17 № 37371
+M = [int(i) for i in open('17.txt')]
+c = 0
+maxim = -9999
+for i in range(len(M) - 1):
+    for j in range(i + 1, len(M)):
+        if (max(M[i], M[j]) - min(M[i], M[j])) % 60 == 0:
+            c += 1
+            maxim = max(maxim, max(M[i], M[j]) - min(M[i], M[j]))
+print(c, maxim)
 '''
 # endregion Домашка: ******************************************************************
 
 
 # region Урок: ******************************************************************
 
-# Типы задач 17 номера:
-
 M = [1, 2, 3, 4, 5]
-# 1. Назовём парой два идущих подряд элемента последовательности.
-# 12 23 34 45
 '''
-for i in range(0, len(M)-1):
-    print(f'{M[i]}{M[i+1]}', end=' ')
-print('\n')
+print(M[3])
+print(M[3:])  # все что справа
+print(M[:3])  # все что слева
+print(M[::-1])
+
+# for i in range(len(M)-1):
+#     print(f'{M[i]}{M[i+1]}', end=' ')
+
+for i in range(len(M)-1):
+    print(M[i:i+2], end=' ')
 '''
-# 2. Назовём тройкой три идущих подряд элемента последовательности.
-# 123 234 345
-'''
-for i in range(0, len(M)-2):
-    print(f'{M[i]}{M[i+1]}{M[i+2]}', end=' ')
-print('\n')
-'''
+
 # 3. В данной задаче под парой подразумевается два различных элемента последовательности.
 # 12 13 14 15
 # 23 24 25
@@ -52,170 +67,74 @@ for i in range(0, len(M)):
     for j in range(i+1, len(M)):
         print(f'{M[i]}{M[j]}', end=' ')
     print()
- 
+
 for i in range(0, len(M)):
     for j in range(0, len(M)):
         if i < j:
             print(f'{M[i]}{M[j]}', end=' ')
     print()
+
+for x in M:
+    for y in M:
+        if x < y:
+            print(f'{x}{y}', end=' ')
+    print()
 '''
 
 
-# Теория для работы с txt файлами
-'''
-print(open('17.txt', 'r').read())
-
-print(open('17.txt').readline())
-
-print(open('17.txt').readlines())
-'''
-
-# with open('17.txt', 'r') as f:
-#     M = [int(i) for i in f]
-
-
-# Тип 17 № 38951
-# Файл содержит последовательность неотрицательных целых чисел, не превышающих 10 000. Назовём парой
-# два идущих подряд элемента последовательности. Определите количество пар, в которых хотя бы один из двух элементов
-# делится на 3, а их сумма делится на 5. В ответе запишите два числа: сначала количество найденных пар,
-# а затем – максимальную сумму элементов таких пар.
-'''
-M = [int(i) for i in open('17.txt')]
-count = 0
-maxi = -9999
-for i in range(0, len(M)-1):
-    if M[i] % 3 == 0 or M[i+1] % 3 == 0:
-        if (M[i] + M[i+1]) % 5 == 0:
-            count += 1
-            maxi = max(maxi, M[i] + M[i+1])
-print(count, maxi)
-'''
-# Ответ: 635 19730
-
-
-# Тип 17 № 40992
-# Файл содержит последовательность неотрицательных целых чисел, не превышающих 10 000. Назовём парой
-# два идущих подряд элемента последовательности. Определите количество пар, в которых хотя бы один из двух элементов
-# делится на 5 и хотя бы один из двух элементов меньше среднего арифметического всех элементов последовательности,
-# значение которых нечетно. В ответе запишите два числа: сначала количество найденных пар, а затем — максимальную
-# сумму элементов таких пар.
-
-# A = [int(i) for i in open('17.txt') if int(i) % 2 != 0]
-# sred = sum(A) / len(A)
-'''
-M = [int(i) for i in open('17.txt')]
-A = [i for i in M if i % 2 != 0]
-sred = sum(A) / len(A)
-
-# summ = 0
-# kol = 0
-# for x in M:
-#     if x % 2 != 0:
-#         summ += x
-#         kol += 1
-# sred = summ / kol
-
-count = 0
-maxi = -9999
-for i in range(0, len(M)-1):
-    if M[i] % 5 == 0 or M[i+1] % 5 == 0:
-        if M[i] < sred or M[i+1] < sred:
-            count += 1
-            maxi = max(maxi, M[i] + M[i+1])
-print(count, maxi)
-'''
-# Ответ: 1531 14932
-
-
-
-# Тип 17 № 37347
-# В файле содержится последовательность из 10000 целых положительных чисел. Каждое число не
-# превышает 10000. Определите и запишите в ответе сначала количество пар элементов последовательности, для которых
-# произведение элементов не кратно 14, затем максимальную из сумм элементов таких пар. В данной задаче под парой
-# подразумевается два различных элемента последовательности. Порядок элементов в паре не важен.
-'''
-M = [int(i) for i in open('17.txt')]
-count = 0
-maxi = -9999
-for i in range(0, len(M)):
-    for j in range(i+1, len(M)):
-        if (M[i] * M[j]) % 14 != 0:
-            count += 1
-            maxi = max(maxi, M[i] + M[j])
-print(count, maxi)
-'''
-# Ответ: 40436496 19999
-
-# s = '1213424'
-# print(s.split.__doc__)
-
-'''
-Return a list of the substrings in the string, using sep as the separator string.
-
-  sep
-    The separator used to split the string.
-
-    When set to None (the default value), will split on any whitespace
-    character (including \\n \\r \\t \\f and spaces) and will discard
-    empty strings from the result.
-  maxsplit
-    Maximum number of splits (starting from the left).
-    -1 (the default value) means no limit.
-
-Note, str.split() is mainly useful for data that has been intentionally
-delimited.  With natural text that includes punctuation, consider using
-the regular expression module.Return a list of the substrings in the string, using sep as the separator string.
-
-  sep
-    The separator used to split the string.
-
-    When set to None (the default value), will split on any whitespace
-    character (including \\n \\r \\t \\f and spaces) and will discard
-    empty strings from the result.
-  maxsplit
-    Maximum number of splits (starting from the left).
-    -1 (the default value) means no limit.
-
-Note, str.split() is mainly useful for data that has been intentionally
-delimited.  With natural text that includes punctuation, consider using
-the regular expression module.
-'''
-
-
-A = [1, 2, 3]
-B = [4, 5, 6]
-C = A + B
-print(C)
-# [1, 2, 3, 4, 5, 6]
-
-
-
-# Тип 9 № 35467
+# Тип 9 № 33088
 # Электронная таблица содержит результаты ежечасного измерения температуры воздуха на протяжении трёх
-# месяцев. Определите, сколько раз за время измерений результат очередного измерения оказывался выше результата
-# предыдущего на 2 и более градусов.
-
-M = []
+# месяцев. Определите, сколько раз за время наблюдений суточные колебания температуры (разность между максимальной и
+# минимальной температурой в течение суток) были меньше 14 градусов.
+'''
+count = 0
 for s in open('9.txt'):
-    M += [float(i) for i in s.replace(',', '.').split()]
-
-count = 0
-for i in range(0, len(M)-1):
-    if M[i+1] - M[i] >= 2:
-        count += 1
-print(count)
-
-
-'''
-M = [3, 4, 7, 19, 23, 26, 4, 5, 9]
-count = 0
-for i in range(0, len(M)-1):
-    if M[i+1] - M[i] >= 2:
+    M = [float(i) for i in s.replace(',', '.').split()]
+    if max(M) - min(M) < 14:
         count += 1
 print(count)
 '''
+# Ответ: 6
+
+'''
+M = [1, 4, 5,23,43 ,423]
+M.sort()
+print(M)
+
+A = [1, 5, 6,34,4 ,5,45, 4]
+print(sorted(A))
+print(A)
+'''
 
 
+# КЕГЭ № 6897 OpenFIPI (Уровень: Средний)
+# Откройте файл электронной таблицы, содержащей в каждой строке четыре натуральных числа.
+# Определите количество строк таблицы, содержащих числа, для которых выполнены оба условия:
+#
+# – наибольшее из четырёх чисел меньше суммы трёх других;
+#
+# – четыре числа нельзя разбить на две пары чисел с равными суммами.
+#
+# В ответе запишите только число.
+
+import itertools
+count = 0
+for s in open('9.txt'):
+    # M = sorted([int(i) for i in s.split()])
+    # if M[3] < M[0] + M[1] + M[2]:
+    M = [int(i) for i in s.split()]
+    if max(M) < sum(M) - max(M):
+        # if all(A[0] + A[1] != A[2] + A[3] for A in itertools.permutations(M, 4)):
+        # if all(sum(A[:2]) != sum(A[2:]) for A in itertools.permutations(M, 4)):
+        if any(sum(A[:2]) == sum(A[2:]) for A in itertools.permutations(M, 4)) == False:
+            count += 1
+print(count)
+
+# Ответ: 2396
+
+
+# M = [int(i) for i in open('9.txt').read().split()]
+# print(M)
 
 
 
@@ -223,6 +142,6 @@ print(count)
 # endregion Урок: ******************************************************************
 
 
-# todo: Сергей = [2, 5, 8, 12, 14, 15, 16, 17, 23, 25]
-# на прошлом уроке: Разбирали 17 номера и затронули (чуть-чуть) 9 номер
+# todo: Сергей = [2, 5, 8, 9, 12, 14, 15, 16, 17, 23, 25]
+# на прошлом уроке: Разбирали 9-ые номера работа с txt файлами
 # на следующем уроке:
