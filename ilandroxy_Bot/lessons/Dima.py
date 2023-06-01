d1cd39435678147c806b5a68115567b3198323bd
Ilya Andrianov <ilandroxxy@gmail.com>
2023-06-02 01:03:22 +0700

    Занятия от `01.06.2023`:
    - ВасилийRemire #пробник Хороший парень из Москвской области, хочет поступать в Москву и работать разработчиком. В программирование пока что без опыта, но уже пробовал решать первые 13 задач из ЕГЭ.
    
    - Александр2 Дорешали вариант Ишимов 1, начиная с 18 номера и все до 25-го.
    
    - Дмитрий Прорешивали Джобс 09.01.2023, успели прорешать первые 15 номеров.
    
    - Всеволод Прорешивали вариант с апробации от 17.05, успели разобрать первые 13 задач.

--- ilandroxy_Bot/lessons/Dima.py
+++ ilandroxy_Bot/lessons/Dima.py
@@ -8,57 +8,11 @@
 
 # region Урок: ******************************************************************************
 
-'''
-count = 0
-for s in open('9.txt'):
-    m = [int(i) for i in s.split()]
-    if len(m) == len(set(m)):
-        m = sorted(m)
-        if 3 * (m[0] + m[4]) >= 2 * (m[1] + m[2] + m[3]):
-            count += 1
-print(count)
-'''
-'''
-count = 0
-mx_s = 0
-m = [int(i) for i in open('17.txt')]
-n = [x for x in m if len(str(x)) == 3 and abs(x) % 10 == 5]
-for i in range(len(m)-1):
-    if len(str(m[i])) == 3 or len(str(m[i+1])) == 3:
-        if (m[i] + m[i+1]) % min(n) == 0:
-            count += 1
-            mx_s = max(mx_s, m[i] + m[i+1])
-print(count, mx_s)
-'''
-'''
-s = 'NN OO PP NO ON PO OP NP PN'.split()
-m = open('24.txt').readline()
-for x in s:
-    m = m.replace(x, '* *')
-n = [len(i) for i in m.split()]
-print(max(n))
-'''
-
-
-'''
-count = 0
-mx_s = 0
-s = open('26.txt')
-m = [int(i) for i in s]
-for i in m:
-    for j in m:
-        if i > j:
-            if i % 2 != 0 and j % 2 != 0:
-                if (i + j) // 2 in m:
-                    count += 1
-                    mx_s = max(mx_s, (i + j) / 2)
-                    print(count, mx_s)
-'''
 
 # endregion Урок: ******************************************************************************
 
 
-# todo:    Дмитрий   = [1, 2, 3, 4, 5+, 6, 7, 8+, 9.1, 11, 12, 13, 14+, 15+, 16+, 17, 18, 19-21, 22, 23, 24, 25.2]
+# todo:    Дмитрий   = [1, 2, 3, 4, 5+, 6, 7, 8+, 9.1, 10, 11, 12, 13, 14+, 15+, 16+, 17, 18, 19-21, 22, 23, 24, 25.2]
 # todo: Дмитрий КЕГЭ = [5, 14, 16, 25]
-# на прошлом уроке: Прорешиваи .txt задачи с демоверсии варианта от 17.05 числа. Затронули 26 номера. И Повторили 18 номерс Ладьей.
+# на прошлом уроке: Прорешивали Джобс 09.01.2023, успели прорешать первые 15 номеров.
 # на следующем уроке:
