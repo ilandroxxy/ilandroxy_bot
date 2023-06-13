1c9a983d39b6fdc7c927d1c8461c36c1a3e1c686
Ilya Andrianov <ilandroxxy@gmail.com>
2023-06-13 14:07:28 +0700

    Занятия `Понедельник 12.06.2023`:
    - #Бронислав Половину урока прорешивали 2 номера (составление таблиц) и разобрали 3, 22 номера через ВПР (записали видео).
    
    - #Василий Разбирали домашку и вопросы по ней: 17, 14 номера. Немного поговорили про 25-ые.
    
    - #Эмиль Добивали 9-ые номера и начали говорить про 25-ые.
    
    - #Данил Посмотрели на решение 25 через fnmatch
    
    - #Валерия Закрывали задачи с вопросами из домашней работы: 14, 8, 5, 6, 25 через новое решение fnmatch
    
    - #Иван Прорешали два вопроса по 5 номеру и разобрали 25 номера с новым решением через fnmatch
    
    - #Татьяна Посмотрели на решение 25 через fnmatch и начали решать вариант Вебиум, номера: 5, 6, 8, 11, 14, 15

--- ilandroxy_Bot/lessons/TikTok_Shorts.py
+++ ilandroxy_Bot/lessons/TikTok_Shorts.py
@@ -1,7 +1,18 @@
 # РЕШУ ЕГЭ Тип 14 № 8664
 # Сколько единиц содержится в двоичной записи
 # значения выражения: 8**2020 + 4**2017 + 26 - 1?
-
+'''
 x = 8**2020 + 4**2017 + 26 - 1
 s = bin(x)[2:]
 print(s.count('1'))
+'''
+
+s = '47236ergh472h36'
+summ = 0
+for x in s:
+    if x.isdigit():
+        summ += int(x)
+print(summ)
+
+summ = sum([int(x) for x in '47236ergh472h36' if x.isdigit()])
+print(summ)
