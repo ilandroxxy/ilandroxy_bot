2daaf5ee5c724ece27159510a4fd54a1375ee4e7
Ilya Andrianov <ilandroxxy@gmail.com>
2023-09-22 11:24:03 +0300

    Занятия Четверг 21.09.2023:
    - #Александра Разбирали базы данных SQLite3 - писали скрипт на создание db, создание таблицы, заполнение таблицы.
    
    - #Анастасия Поболтали, разобрали домашку на тему "Типы данных и базовая арифметика".
    
    - #Мария Продолжаем работу с репазиторием. Попробовали команды: echo >, rmdir, rm, git status, git add, git commit. Остановились на git push, так как была проблема с "токеном".
    
    - #ВасилийRemire Разбирали все типы 15-ых номеров.
    
    - #Дарья #пробник Классная, заряженная девушка. Хочет сдавать информатику, чтобы поступить в ИТМО и работать в gamedev.

--- ilandroxy_Bot/lessons/Sasha.py
+++ ilandroxy_Bot/lessons/Sasha.py
@@ -7,42 +7,22 @@
 
 # region Урок: ************************************************************
 
-# Импортирование необходимых библиотек
-import pandas as pd  # Импорт библиотеки pandas для работы с данными в виде таблицы
-from sklearn.model_selection import train_test_split  # Импорт функции train_test_split из библиотеки scikit-learn для разделения данных на обучающую и тестовую выборки
-from sklearn.ensemble import RandomForestClassifier  # Импорт класса RandomForestClassifier из библиотеки scikit-learn для создания и обучения модели случайного леса
-from sklearn.metrics import accuracy_score  # Импорт функции accuracy_score из библиотеки scikit-learn для оценки производительности модели
-import matplotlib.pyplot as plt  # Импорт библиотеки matplotlib.pyplot для визуализации данных
+import sqlite3 as sq
 
-# Загрузка набора данных
-data = pd.read_csv('high_diamond_ranked_10min.csv')  # Загрузка данных из CSV файла в pandas DataFrame
 
-# Определение признаков и целевой переменной
-X = data.drop(['gameId', 'blueWins'], axis=1)  # Определение признаков, исключая столбцы 'gameId' и 'blueWins'
-y = data['blueWins']  # Определение целевой переменной 'blueWins'
+db = sq.connect("sqlite.db")
+cur = db.cursor()
 
-# Разделение данных на обучающую и тестовую выборки
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)  # Разделение данных на обучающую и тестовую выборки в соотношении 80:20
+cur.execute("""CREATE TABLE IF NOT EXISTS users(
+                            user_id INTEGER,
+                            username TEXT,
+                            name TEXT)
+                """)
+db.commit()
+cur.execute("INSERT INTO users VALUES(?, ?, ?)", (1232142, 'ilandroxxy', 'Ilya'))
+db.commit()
 
-# Создание и обучение модели случайного леса
-model = RandomForestClassifier()  # Создание объекта модели случайного леса
-model.fit(X_train, y_train)  # Обучение модели на обучающей выборке
-
-# Оценка производительности модели на тестовой выборке
-y_pred = model.predict(X_test)  # Предсказание целевой переменной на тестовой выборке
-accuracy = accuracy_score(y_test, y_pred)  # Вычисление точности модели на тестовой выборке
-
-# Определение важности признаков
-importance = model.featureimportances  # Получение важности признаков из обученной модели
-
-# Визуализация важности признаков в виде графика
-plt.bar(X.columns, importance)  # Создание столбчатой диаграммы с признаками по оси X и их важностью по оси Y
-plt.xlabel("Features")  # Название оси X
-plt.ylabel("Importance")  # Название оси Y
-plt.title("Feature Importance")  # Заголовок графика
-plt.xticks(rotation=90)  # Поворот названий признаков на 90 градусов
-plt.show()  # Отображение графика
-
+# endregion db_start()
 
 
 # endregion Урок: ************************************************************
