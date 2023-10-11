d0c638c0578a7d513830437b76829e4e20c60d07
Ilya Andrianov <ilandroxxy@gmail.com>
2023-10-11 16:12:52 +0700

    Занятия Вторник 10.10.2023:
    - #Екатерина Очень плотно нарешивали задачи 2 номера ЕГЭ, Катя пробовала решать сама.
    
    - #Анастасия Очень плотно прошлись по библиотекам питона и разобрали задачи 6-го номера.
    
    - #Артур Плотно обсудили циклы while и for. Написали программу для перевода в разные системы счисления/проверку паролей.
    
    - #Геля #Артём #Лиза #Олег Порешали задачи на turtle 6 номер, повторили работу с библиотеками. Так как у нас осталось время, то поговорили про теорию множеств и строковый тип данных.
    
    - #Варя Разобрали домашние задачки и условные операторы.

--- ilandroxy_Bot/lessons/K_Brun.py
+++ ilandroxy_Bot/lessons/K_Brun.py
@@ -1,107 +1,92 @@
 
 # region Домашка: ******************************************************************
 
-'''
-s = input()
-n = int(input())
-for _ in range(n):
-    print(s)
-'''
+# # ((x → y ) ∧ (y → w)) ∨ (z ≡ ( x ∨ y))
+# F=((X <= Y) and (y<=w)) or (z==(x or y))
+# https://inf-ege.sdamgia.ru/problem?id=15787
+#
+# # (x ≡ ( w ∨ y)) ∨ ((w → z ) ∧ (y → w))
+# F=(x==(w or y)) or ((w<=z) and (y<=w))
+# https://inf-ege.sdamgia.ru/problem?id=15814
+#
+# # (z ∧ y) ∨ ((x → z ) ≡ (y → w))
+# F=(z and y) or ((x<=z)==(y<=w))
+# https://inf-ege.sdamgia.ru/problem?id=15939
+#
+# # ((y → x) ≡ (x → w)) ∧ (z ∨ x)
+# F= ((y <= x) == (x <= w)) and (z or x)
+# https://inf-ege.sdamgia.ru/problem?id=16431
+#
+# # (x≡¬y)→(z≡(y∨w))
+# F=(x == (not y))<=(z == (y or w))
+# https://inf-ege.sdamgia.ru/problem?id=16878
+#
+# # ((x ∧ y) ∨ (y ∧ z)) ≡ ((x → w) ∧ (w → z))
+# F=((x and  y)or(y and z)) == ((x<=w) and (w<=z))
+# https://inf-ege.sdamgia.ru/problem?id=17320
+#
+# # (z ∧ y) ∨ ((x → z ) ≡ (y → w))
+# F=(z and y) or ((x<=z) == (y<=w))
+# https://inf-ege.sdamgia.ru/problem?id=15939
+#
+# # (x ∧ ¬y) ∨ (y ≡ z ) ∨ w
+# F=(x and (not y)) or (y == z) or w
+# https://inf-ege.sdamgia.ru/problem?id=15970
+#
+# #  ((x → y) ≡ (y → z)) ∧ (y ∨ w)
+# F=((x<=y) == (y<=z)) and (y or w)
+# https://inf-ege.sdamgia.ru/problem?id=16377
+#
+# # ((y → x) ≡ (x → w)) ∧ (z ∨ x)
+# F=((y<=x) == (x<=w)) and (z or x)
+# https://inf-ege.sdamgia.ru/problem?id=16431
 
-'''
-for n in range(int(input() + 1)):
-    print(f'Квадрат числа {n} равен {n**2}')
-'''
 
-'''
-while True:
-    x = int(input())
-    if x % 7 == 0:
-        print(x)
-    else:
-        break
-'''
-'''
-R = []
-while True:
-    x = int(input())
-    if x >= 0:
-        R.append(x)
-    else:
-        break
-print(sum(R))
-'''
+x, y, z, w = 1, 1, 1, 1
+# ¬(((x → y ∧ w) ∧ (z → x ∨ y)) ≡ w)
+F = not(((x <= (y and w)) and (z <= (x or y))) == w)
 
-'''
-count = 0
-while True:
-    x = int(input())
-    if x < 0 or x > 55:
-        break
-    if x == 5:
-        count += 1
-print(count)
-'''
-
-'''
-ALPHABET = sorted('0123456789QWERTYUIOPASDFGHJKLZXCVBNM')
-# Напишем маленький калькулятор систем счисления:
-while True:
-    case = input('\ncase 1: Перевод из 10-ной в n-ную систему счисления \n'
-                 'case 2: Перевод из n-ной в 10-ную систему счисления \n'
-                 'case 3: Перевод из k-ой в n-ную систему счисления \n'
-                 'case 0: exit \n')
-    if case == '1':
-        x = int(input('Введите 10-ное число: '))
-        x2 = x
-        n = int(input('Ведите n-ную систему счисления: '))
-        s = ''
-        while x2 > 0:
-            s += ALPHABET[x2 % n]
-            x2 //= n
-        s = s[::-1]
-        print(f'Результат перевода числа {x} из 10-ной в {n}-ную систему: {s}')
-
-    elif case == '2':
-        n = int(input('Ведите n-ную систему счисления: '))
-        s = input(f'Введите число в {n}-ной системе счисления: ')
-        x = int(s, n)  # то это встроенная функция для перевода из n-ной системы счисления в 10-ную
-        print(f'Результат перевода числа {s} из {n}-ной в 10-ную систему: {x}')
-
-    elif case == '3':
-        k = int(input('Ведите k-ую систему счисления: '))
-        c = input(f'Введите число в {k}-ой системе счисления: ')
-        x = int(c, k)
-
-        n = int(input('Ведите n-ную систему счисления: '))
-        s = ''
-        while x > 0:
-            s += ALPHABET[x % n]
-            x //= n
-        s = s[::-1]
-        print(f'Результат перевода числа {c} из {k}-ой в {n}-ную систему: {s}')
-
-    elif case == '0':
-        break
-'''
 # endregion Домашка: ******************************************************************
 
 
 # region Урок: ******************************************************************
 
-# Тип 2 №16805
-# Логическая функция F задаётся выражением (¬x≡z)→(y≡(w∨x)).
-# Дан частично заполненный фрагмент, содержащий неповторяющиеся строки таблицы истинности функции F.
-# Определите, какому столбцу таблицы истинности соответствует каждая из переменных x, y, z, w.
-
+# Тип 2 №56502
+# Логическая функция F задаётся выражением:
+# ((x→y)∨(z→w))∧((z≡y)→(w≡x))
+'''
 print('x y z w F')
 for x in range(2):
     for y in range(2):
         for z in range(2):
-            for w in [0, 1]:
-                # F = ((not x) == z) <= (y == (w or x))
-                F = ((not x) == z) <= (y == (w or x))
-                if F == False:
+            for w in range(2):
+                F = ((x <= y) or (z <= w)) and ((z == y) <= (w == x))
+                if not F:
+                    print(x, y, z, w, int(F))
+
+'''
+
+# Тип 2 №51971
+# Логическая функция F задаётся выражением:
+# (x≡¬y)→((z→¬w)∧(w→y))
+'''
+print('x y z w F')
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            for w in range(2):
+                F = (x == (not y)) <= ((z <= (not w)) and (w <= y))
+                print(x, y, z, w, int(F))
+'''
+
+#  ((y → z) ∨ (¬x ∧ w)) ≡ (w ≡ z)
+print('x y z w F')
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            for w in range(2):
+                F = ((y <= z) or ((not x) and w)) == (w == z)
+                if F:
                     print(x, y, z, w, int(F))
 
 # endregion Урок: ******************************************************************
--- ilandroxy_Bot/lessons/wqcdix52.py
+++ ilandroxy_Bot/lessons/wqcdix52.py
@@ -1,73 +1,224 @@
 # region Домашка: ******************************************************************
 
-'''
-a = int(input())
-b = int(input())
-c = int(input())
-print(a + b + c)
-'''
-
-'''
-n = len(set(list([1, 2, 3])))
-print(n)
-'''
 # endregion Домашка: ******************************************************************
 
 
 # region Урок: ******************************************************************
 
-# Условные операторы (ветвление): if, elif, else
-'''
-x = -5
-y = 2
+# Циклы while и for
 
-if x > 0 and y > 0:  # if (если) - главное условие
-    print('Первая четверть')
-elif x < 0 and y > 0:
-    print('Вторая четверть')
-elif x < 0 and y < 0:
-    print('Третья четверть')
-elif x > 0 and y < 0:  # elif (иначе если) - любое промежуточное условие
-    print('Четвертая четверть')
-else:  # else (иначе) - противопостовление всем предыдущим условиям
-    print('Лежит на осях')
-'''
+# Цикл - это какое-то повторяющееся действие
 
-# Каскадлые условные операторы
-'''
-x = int(input('x: '))
-y = int(input('y: '))
+# for цикл по типу: "повтори n раз", "пробеги от А до Б"
 
-if x > 0:
-    if y > 0:  # x > 0 and y > 0
-        print('Первая четверть')
-    else:  # x > 0 and y <= 0
-        print('Четвертая четверть')
-else:
-    if y > 0:  # x <= y and y > 0
-        print('Вторая четверть')
-    else:  # x <= 0 and y <= 0
-        print('Третья четверть')
+# Работа с циклом for через функцию range()
+'''
+for x in range(10):  # range(START=0, STOP=10-1, STEP=1)
+    print(x, end=' ')  # 0 1 2 3 4 5 6 7 8 9
+print()
+
+for x in range(2, 10):  # range(START=2, STOP=10-1, STEP=1)
+    print(x, end=' ')  # 2 3 4 5 6 7 8 9
+print()
+
+for x in range(2, 10, 2):  # range(START=2, STOP=10-1, STEP=2)
+    print(x, end=' ')  # 2 4 6 8
+print()
+
+for x in range(2, 10+1, 2):  # range(START=2, STOP=10+1-1, STEP=2)
+    print(x, end=' ')  # 2 4 6 8
+print()
+
+
+for x in range(10, 0, -1):  # range(START=10, STOP=0+1, STEP=-1)
+    print(x, end=' ')  # 10 9 8 7 6 5 4 3 2 1
+print()
 '''
 
+# А зачем этот range() нужен? Он нужен для того, чтобы брать элементы списка (и других) или менять элементы списка
+'''
+# i   0    1    2    3    4
+M = ['a', 'b', 'c', 'd', 'e']
+# -i -5   -4   -3   -2   -1
 
-# Год является високосным, если его номер кратен 4, но не кратен 100, или если он кратен 400.
+print(M[0], M[-1])  # a e - взяли первый и последний элементы
 
-year = int(input())
+s = 'abcde'
+print(s[0], s[-1])  # a e - взяли первый и последний элементы
 
-if year % 4 == 0:
-    print('Кратен')
-elif year % 4 != 0:   # != - не равно
-    print('Не кратен')
 
-# Логические связки: and. or, not
+for i in range(len(M)):  # len(M) - это длина списка. то есть кол-во его элементов
+    # print(i, end=' ')   # 0 1 2 3 4
+    print(M[i], end=' ')  # a b c d e
+print()
 
-a = 6
-b = -7
-if a > 0 and b > 0:
-    print('ДА 1')
-if a > 0 or b > 0:
-    print('Да 2')
+for i in range(len(s)):  # len(M) - это длина списка. то есть кол-во его элементов
+    # print(i, end=' ')   # 0 1 2 3 4
+    print(s[i], end=' ')  # a b c d e
+print()
+
+
+# В списках можно менять значения элементов через индексы
+for i in range(len(M)):
+    M[i] = M[i] * i
+print(M)  # ['', 'b', 'cc', 'ddd', 'eeee']
+'''
+
+# Можно брать элементы списков напрямую из коллекции (последовательности)
+'''
+M = ['a', 'b', 'c', 'd', 'e']
+s = 'abcde'
+
+for x in M:
+    print(x, end=' ')  # a b c d e
+print()
+
+for x in s:
+    print(x, end=' ')  # a b c d e
+print()
+
+L = [234, 34, 564, 23, 45, 23, 12, 343, 23, 543]
+
+for x in L:  # возьмите только четные элементы
+    if x % 2 == 0:
+        print(x, end=' ')   # 234 34 564 12
+print()
+
+for x in L:  # возьмите все трехзначные числа
+    if len(str(x)) == 3:
+        print(x, end=' ')   # 234 564 343 543 
+print()
+'''
+
+
+# while цикл по типу: "делай ПОКА условие выполняется", "бесконечный цикл"
+'''
+for x in range(2, 10+1, 2):  # range(START=2, STOP=10+1-1, STEP=2)
+    print(x, end=' ')  # 2 4 6 8
+print()
+
+i = 2
+while i <= 10:
+    print(i, end=' ')  # 0 1 2 3 4 5 6 7 8 9
+    i += 2  # i = i + 1 и по аналогии: -= /= //= **= *= %=
+print()
+'''
+
+'''
+# Перевод из 10-ной в 2-ную (n-ную систему счисления)
+x = 8
+M = []
+while x > 0:
+    M.append(x % 2)
+    x //= 2
+M.reverse()
+print(M)
+
+print(int('1000', 2))   # 8
+
+# Аналогичная задача, но хотим получить в результате строку
+
+x = 8
+s = ''
+while x > 0:
+    s += str(x % 2)
+    x //= 2
+s = s[::-1]  # срез в обратном порядке
+print(s)
+'''
+
+# Универсальная программа для перевода из 10-ной в n-ную
+'''
+import string
+alphabet1 = string.digits + string.ascii_uppercase
+print(alphabet1)  # 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ
+
+alphabet2 = sorted('0123456789QWERTYUIOPASDFGHJKLZXCVBNM')
+
+x = int(input('Введите 10-ное число: '))
+n = int(input('Введите n-ную систему счисления: '))
+s = ''
+while x > 0:
+    s += alphabet1[x % n]
+    x //= n
+s = s[::-1]
+print(f'Результат перевода: {s}')
+'''
+
+# Бесконечный цикл
+'''
+k = 1
+while True:
+    print(k)
+    k += 1
+'''
+
+'''
+alphabet = sorted('0123456789QWERTYUIOPASDFGHJKLZXCVBNM')
+while True:
+    case = input('\n\n'
+                 'case 1: Перевод из 10-ной в n-ную систему счисления \n'
+                 'case 2: Перевод из n-ной в 10-ную систему счисления \n'
+                 'case 3: Перевод из n-ной в k-ную систему счисления \n'
+                 'case 0: exit \n')
+
+    if case == '1':
+        x = int(input('Введите 10-ное число: '))
+        n = int(input('Введите n-ную систему счисления: '))
+        s = ''
+        while x > 0:
+            s += alphabet[x % n]
+            x //= n
+        s = s[::-1]
+        print(f'Результат перевода: {s}')
+
+    elif case == '2':
+        n = int(input('Введите n-ную систему счисления: '))
+        s = input(f'Введите число {n}-ной системе счисления: ')
+        x = int(s, n)
+        print(f'Результат перевода числа {s} из {n}-ной системы счисления: {x}')
+
+    elif case == '3':
+        pass
+
+    elif case == '0':
+        break
+'''
+
+import random
+import time
+
+Err = ['Не верно', 'Повторите попытку', 'Ошибочно',
+       'Попробуйте еще раз', 'Неправильно',
+       'Повторите пожалуйста', 'Неверный ответ',
+       'Попытайтесь снова', 'Неудачно', 'Пробуйте снова']
+
+
+# Маленький пример скрипта на проверку пароля от пользователя
+'''
+pas = 'qwerty'
+password = input('Введите пароль для входа: ')
+count = 1
+while True:
+    if pas == password:
+        print('Welcome!')
+        break
+    password = input(f'{random.choice(Err)}: ')
+    count += 1
+    if count == 3:
+        print('Слишком много попыток, пройдите проверку на робота решив пример: ')
+        a = random.randint(0, 100)
+        b = random.randint(0, 100)
+        x = int(input(f'{a} + {b} = '))
+        if x == a + b:
+            count = 0
+            print('Проверка успешно пройдена! ')
+        else:
+            print('Подозрительная активность, попробуйте через 5 минут: ')
+            time.sleep(5*60)
+
+print('Тут программа')
+'''
 
 # endregion Урок: ******************************************************************
 
