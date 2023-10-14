80629b8554b6c88294380f8292d8e9907f03998d
Ilya Andrianov <ilandroxxy@gmail.com>
2023-10-14 22:58:54 +0700

    Занятия Суббота `14.10.2023`:
    - #Янина В боте проделали работы по кнопке «сдать домашку», написал  обработчик как обычной кнопки так и inline. Описали команду «отменить»
    
    - #Андрей Восстановили доступ к репозиторию, создали нормальный гит проект и сделали коммиты.
    
    - #Лера Разобрали теорию 2-го номера и прорешали задачи всех типов.
    
    - #Сева Посмотрели на задачки из дополнительной домашки по циклам, разобрали много задач2-го номера и немного поговорили про 6-ой номер (решили один номер двумя способами).
    
    - #Саяд Поговорили про проводник, скачали Pychram и запустили первый проект.

--- ilandroxy_Bot/lessons/A_95_4elovek.py
+++ ilandroxy_Bot/lessons/A_95_4elovek.py
@@ -1,46 +1,128 @@
+# region Домашка: ************************************************************
 
-# region Домашка: ***************************************************************
 '''
-# Как есть:
-count = 0
-while count <= 9:
-    print("Век живи - век учись.")
-    count += 1
-
-# Как правильно:
-s = input()
 n = int(input())
-for i in range(n):
-    print(s)
+summ = 0
+for x in range(1, n+1):
+    if (x**2) % 10 in (2, 5, 8):
+        summ += x
+print(summ)
 '''
+
+'''
+n = int(input())  # 123456
+M = []
+while n > 0:
+    M.append(n % 10)
+    n //= 10
+M.reverse()
+print(M[2])
+'''
+
+'''
+n = int(input())
+s = str(n)
+print(s[2])
+'''
+
+
 # endregion Домашка: ************************************************************
 
 
 # region Урок: ******************************************************************
-
-# (¬x ∨ y ∨ z) ≡ (¬y ∧ z ∧ w)
 
 #  ¬x     --->    (not x)  - инверсия (отрицание)
 # x ∧ y   --->    x and y  - конъюнкция (логическое умножение)
 # x ∨ y   --->    x or y   - дизъюнкция (логическое сложение)
 # x → y   --->    x <= y   - импликация
 # x ≡ y   --->    x == y   - тождество (сравнение)
-
-# Тип 2 №27228
-# Логическая функция F задаётся выражением (¬x ∨ y ∨ z) ≡ (¬y ∧ z ∧ w).
-# На рисунке приведён частично заполненный фрагмент таблицы истинности функции F,
-# содержащий неповторяющиеся строки. Определите, какому столбцу таблицы истинности
-# функции F соответствует каждая из переменных x, y, z, w.
-
-print('x y z w')
-for x in range(2):
+'''
+print('x y z w F')
+for x in range(2):  # range(START=0, STOP=2-1, STEP=1)
     for y in range(2):
         for z in range(2):
             for w in range(2):
-                # (¬x ∨ y ∨ z) ≡ (¬y ∧ z ∧ w)
-                F = ((not x) or y or z) == ((not y) and z and w)
+                # F = (not(y <= x)) or (z <= w) or (not z)   # №47206  ¬(y→x)∨(z→w)∨¬z
+                # F = ((x <= y) or (z <= w)) and ((z == y) <= (w == x))  # №56502 ((x→y)∨(z→w)) ∧ ((z≡y)→(w≡x))
+                F = (x or (not y)) and (not(w == z)) and w  # Тип 2 №18704  (x ∨ ¬y) ∧ ¬(w ≡ z) ∧ w
                 if F:
-                    print(x, y, z, w)
+                    print(x, y, z, w, int(F))
+'''
+
+# Тип 2 №52173
+# Логическая функция F задаётся выражением: (z≡¬x)→((w→¬y)∧(y→x))
+'''
+print('x y z w F')
+for x in range(2):  # range(START=0, STOP=2-1, STEP=1)
+    for y in range(2):
+        for z in range(2):
+            for w in range(2):
+                F = (z == (not x)) <= ((w <= (not y)) and (y <= x))
+                print(x, y, z, w, int(F))
+'''
+
+# Тип 6 №51975
+# В начальный момент Черепаха находится в начале координат и направлена
+# вверх (вдоль положительного направления оси ординат).
+
+# Черепаха выполнила следующую программу:
+# Повтори 4 [Вперёд 7 Направо 90 Вперёд 7 Налево 90 Вперёд 7 Направо 90]
+#
+# Определите, сколько точек с целочисленными координатами
+# будут находиться внутри области, ограниченной линией, полученной при выполнении данной программы.
+# Точки, расположенные на линии, не учитывать.
+'''
+import turtle as t
+t.left(90)
+t.speed(100)
+l = 20
+
+# Повтори 4 [Вперёд 7 Направо 90 Вперёд 7 Налево 90 Вперёд 7 Направо 90]
+for i in range(4):
+    t.forward(7*l)
+    t.right(90)
+    t.forward(7*l)
+    t.left(90)
+    t.forward(7*l)
+    t.right(90)
+
+t.up()
+for x in range(0, 25):
+    for y in range(-10, 20):
+        t.goto(x*l, y*l)
+        t.dot(2, 'red')
+
+t.done()
+'''
+
+# Вариант 2
+'''
+import turtle as t
+t.left(90)
+t.speed(10)
+l = 20
+
+t.begin_fill()
+for i in range(4):
+    t.forward(7*l)
+    t.right(90)
+    t.forward(7*l)
+    t.left(90)
+    t.forward(7*l)
+    t.right(90)
+t.end_fill()
+
+count = 0
+canvas = t.getcanvas()
+for x in range(-100*l, 100*l, l):
+    for y in range(-100*l, 100*l, l):
+        item = canvas.find_overlapping(x, y, x, y)
+        if len(item) == 1 and item[0] == 5:
+            count += 1
+print(count)  # 204
+
+t.done()
+'''
 
 # endregion Урок: ***************************************************************
 
--- ilandroxy_Bot/lessons/aaaaaad6.py
+++ ilandroxy_Bot/lessons/aaaaaad6.py
@@ -6,212 +6,45 @@
 
 # region Урок: ******************************************************************
 
-# Циклы while и for: Цикл - повторяющееся действие
-
-# Цикл for: "Повтори n раз", "Пробеги от a до b"
+# Тип 2 №38534
+# Миша заполнял таблицу истинности логической функции F
+# ¬(y→(x≡w))∧(z→x)
 '''
-# Работа цикла for с использованием функции range()
-for x in range(10):  # range(START=0, STOP=10-1, STEP=1)
-    print(x, end=' ')  # 0 1 2 3 4 5 6 7 8 9
-print()
-
-for x in range(2, 10):  # range(START=2, STOP=10-1, STEP=1)
-    print(x, end=' ')  # 2 3 4 5 6 7 8 9
-print()
-
-for x in range(2, 10, 2):  # range(START=2, STOP=10-1, STEP=2)
-    print(x, end=' ')  # 2 4 6 8
-print()
-
-for x in range(2, 10+1, 2):  # range(START=2, STOP=10-1, STEP=2)
-    print(x, end=' ')  # 2 4 6 8 10
-print()
-
-for x in range(10, 0, -1):
-    print(x, end=' ')  # 10 9 8 7 6 5 4 3 2 1
-print()
-
-# i   0    1    2    3    4
-M = ['a', 'b', 'c', 'd', 'e']
-# -i -5   -4   -3   -2   -1
-
-s = 'abcde'
-
-print(M[0], M[-1])   # взятие крайнего левого и крайнего правого элементов
-print(s[0], s[-1])  # a e
-
-for i in range(0, len(M)):  # len(M) - это функция возвращающая длину последовательности (кол-во элементов)
-    # print(i, end=' ')  # 0 1 2 3 4
-    print(M[i], end=' ')  # a b c d e
-    # Получаем элементы списка через индексы
-print()
-
-for i in range(len(s)):
-    print(s[i], end=' ')  # a b c d e
-print()
-
-# В чем разница использования индексов через функцию range для списков и строк?
-# Ответ: в списках через индексы можно менять элементы, а в строках/кортежах нельзя
-
-# i   0    1    2    3    4
-M = ['a', 'b', 'c', 'd', 'e']
-# -i -5   -4   -3   -2   -1
-
-for i in range(len(M)):
-    M[i] = M[i] * i
-print(M)  # ['', 'b', 'cc', 'ddd', 'eeee']
-
-
-# Можно обращаться (брать) к элементам последовательности напрямую
-
-M = ['a', 'b', 'c', 'd', 'e']
-s = 'abcde'
-
-for x in M:
-    print(x, end=' ')  # a b c d e
-print()
-
-import random
-L = [random.randint(1, 100) for _ in range(10)]  # [90, 17, 72, 79, 68, 52, 51, 17, 17, 24]
-print(L)
-
-# Задача: взять все четные элементы списка
-for x in L:
-    if x % 2 == 0:
-        print(x, end=' ')
-print()
+print('x y z w F')
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            for w in range(2):
+                F = (not(y <= (x == w))) and (z <= x)
+                if F:
+                    print(x, y, z, w, int(F))
 '''
 
 
-# Цикл while: "Повторяй действие ПОКА условие выполняется", "Бесконечный цикл"
+# Тип 2 №35460  ¬((x ∨ y) → (z ∧ w)) ∧ (x → w).
+# Тип 2 №27371  ((x ∧ ¬y) → (¬z ∨ ¬w))∧((w→x) ∨ y)
 '''
-for x in range(2, 10+1, 2):  # range(START=2, STOP=10-1, STEP=2)
-    print(x, end=' ')  # 2 4 6 8 10 
-print()
-
-i = 2
-while i <= 10:
-    print(i, end=' ')  # 2 4 6 8 10 
-    i += 2
-print()
-'''
-
-
-# Перевод из 10-ной в 2-ную систему счисления
-'''
-x = 8
-M = []
-while x > 0:
-    M.append(x % 2)  # метод списков, который добавляет новый элемент в конец списка
-    x //= 2
-M.reverse()  # это метод списков, разворачивающий все элементы в обратном порядке
-print(M)
+print('x y z w F')
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            for w in range(2):
+                # F = (not((x or y) <= (z and w))) and (x <= w)  # 35460
+                F = ((x and (not y)) <= ((not z) or (not w)) and (w <= x) or y)  # 27371
+                if not F:
+                    print(x, y, z, w, int(F))
 '''
 
-# Перевод из 10-ной в n-ную систему счисления
-'''
-x = int(input())
-n = int(input())
-M = []
-while x > 0:
-    M.append(x % n)  # метод списков, который добавляет новый элемент в конец списка
-    x //= n
-M.reverse()  # это метод списков, разворачивающий все элементы в обратном порядке
-print(M)
-'''
+# Тип 2 №18483
+# Логическая функция F задаётся выражением ((y → w) ≡ (x → ¬z)) ∧ (x ∨ w).
 
-# Перевод из 10-ной в n-ную через строки
-'''
-import string as st
-alphabet1 = st.digits + st.ascii_uppercase
-print(alphabet1)  # 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ
-
-alphabet2 = sorted('0123456789QWERTYUIOPASDFGHJKLZXCVBNM')
-
-x = int(input())
-n = int(input())
-s = ''
-while x > 0:
-    s += alphabet1[x % n]
-    x //= n
-s = s[::-1]  # срез элементов строки в обратном порядке
-print(s)
-'''
-
-
-# break и continue
-'''
-for x in range(100):
-    if x == 50:
-        break
-    if x % 2 != 0:
-        continue
-    print(x)
-'''
-
-# Бесконечные циклы
-'''
-k = 1
-while True:
-    k += 1
-    print(k)
-'''
-
-
-# программа для проверки пароля
-'''
-import random
-import time
-pas = 'qwerty'
-count = 0
-password = input('Введите пароль для проверки: ')
-while True:
-    if pas == password:
-        print('Welcome!')
-        break
-    password = input('Пароль неверный, попробуйте снова: ')
-    count += 1
-    if count == 2:
-        print('Подозрительная попытка входа, пройдите проверку на робота, решив пример: ')
-        a = random.randint(1, 100)
-        b = random.randint(1, 100)
-        x = int(input(f'{a} + {b} = '))
-        if x == a + b:
-            count = 0
-            print('Проверка пройдена успешно!')
-        else:
-            print('Проверка на робота не пройдена, повторите через 5 минут: ')
-            time.sleep(5*60)
-'''
-
-
-'''
-alphabet = sorted('0123456789QWERTYUIOPASDFGHJKLZXCVBNM')
-while True:
-    case = input('\n\ncase 1: Перевод из 10-ной в n-ную: \n'
-                 'case 2: \n'
-                 'case 3: \n'
-                 'case 0: exit \n')
-
-    if case == '1':
-        x = int(input('Введите 10-ное число для перевода: '))
-        n = int(input('Введите систему счисления в которую будем переводить: '))
-        s = ''
-        while x > 0:
-            s += alphabet[x % n]
-            x //= n
-        s = s[::-1]  # срез элементов строки в обратном порядке
-        print(f'Результат перевода: {s}')
-
-    elif case == '2':
-        pass
-
-    elif case == '3':
-        pass
-
-    elif case == '0':
-        break
-'''
+print('x y z w F')
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            for w in range(2):
+                F = ((y <= w) == (x <= (not z))) and (x or w)
+                print(x, y, z, w, int(F))
 # endregion Урок: ******************************************************************
 
 
