af05b95ec65d0cb5dadf76cd59ab24d7ee9e11a0
Ilya Andrianov <ilandroxxy@gmail.com>
2023-10-31 01:26:21 +0700

    Занятия Понедельник `30.10.2023`:
    - #Марго Поговорили про правила оформления ДЗ, разобрали теорию по циклам for, while и условные операторы.
    
    - #Лиза Разобрали 12-ый номер ЕГЭ полностью. Договорились на следующем уроке посмотреть домашки по спискам/строка/12-ому номеру.
    
    - #Евгений Прорешивали домашку по 8.1 и прорешали два номера из 8.2
    
    - #Диана Работали с графиками функции.
    
    - #Марк Разобрали все 15-ые номера. Несколько задач Марк решил сам.
    
    - #ВасилийRemire Василий сам прорешал всю домашку 17.1 - плотно разобрали работу с разными парами и тройками.
    
    - #Дарья Разобрали теорию списков и для примера решили 17-ый, 5-ый номера.

--- ilandroxy_Bot/lessons/vllcrowww.py
+++ ilandroxy_Bot/lessons/vllcrowww.py
@@ -7,91 +7,177 @@
 
 # region Урок: ******************************************************************
 
-# f-сроки
-"""
-weather = 'облачно'
-temperature = int(input('Введите температуру: '))
-# "Сегодня облачно, а температура 24 градуса!"
-print("Сегодня", weather, ", а температура ", temperature, " градуса!")
-print("Сегодня " + weather + ", а температура " + str(temperature) + " градуса!")
-# TypeError: can only concatenate str (not "int") to str
-print("Сегодня {}, а температура {} градуса!".format(weather, temperature))
-print(f"Сегодня {weather}, а температура {temperature} градуса!")
-"""
+# Условные операторы (ветвления): if, elif, else
 
-# Базовая арифметика
 '''
-a = 7
-b = 2
-print(f'{a} + {b} = {a+b} \n'  # \n - переход на новую строку в консоли
-      f'{a} - {b} = {a-b} \n'
-      f'{a} * {b} = {a * b}')
+x = 0
+y = 8
 
-print()  # в пустом принте есть символ '\n' - переход на новую строку
+if x > 0 and y > 0:  # if - ЕСЛИ
+    print('Первая четверть')
+elif x < 0 and y > 0:  # elif - ИНАЧЕ ЕСЛИ
+    print('Вторая четверть')
+elif x < 0 and y < 0:
+    print('Третья четверть')
+elif x > 0 and y < 0:
+    print('Четвертая четверть')
+else:  # else - ИНАЧЕ
+    print('Лежит на осях')
 
-print(f'{a} / {b} = {a/b} - вещественное деление (результат такого деления всегда float) \n'  
-      f'{a} // {b} = {a//b} - только целая часть от деления (без округлений) \n'
-      f'{a} % {b} = {a % b} - остаток от деления (от обыкновенной дроби) ')
-# Проверки из разряда: Кратное ли число? Четное ли число? Делится ли число?
+print('Конец программы')
+'''
 
+# Логические связки: and, or, not
+'''
+flag = True
+print(not flag)  # False
+
+a = 5
+b = -4
+
+if a > 0 and b > 0:
+    print('YES1')
+if a > 0 or b > 0:  # хотя бы одно условие должно выполняться
+    print('YES2')
+
+
+x = 0
+y = 8
+
+if x > 0 and y > 0:  # if - ЕСЛИ
+    print('Первая четверть')
+
+if x > 0:  # вложенность условий
+    if y > 0:
+        print('Первая четверть')
+'''
+
+
+# Задание ака 17-ый номер
+# Выведите на экран только четные числа
+'''
+import random
+M = [random.randint(0, 100) for _ in range(10)]
+for x in M:
+    if x % 2 == 0:
+        print(x, end=' ')
+print()
+'''
+
+# Цикл for: "повтори n раз", "пробеги диапазон чисел от a до b"
+'''
+for i in range(10):  # range(START=0, STOP=10-1, STEP=1)
+    print(i, end=' ')  # 0 1 2 3 4 5 6 7 8 9
 print()
 
+for i in range(2, 10):  # range(START=2, STOP=10-1, STEP=1)
+    print(i, end=' ')  # 2 3 4 5 6 7 8 9
+print()
+
+for i in range(2, 10+1, 2):  # range(START=2, STOP=11-1, STEP=2)
+    print(i, end=' ')  # 2 4 6 8 10
+print()
+
+for i in range(10, 0, -1):  # range(START=10, STOP=0+1, STEP=-1)
+    print(i, end=' ')  # 10 9 8 7 6 5 4 3 2 1
+print()
+
+# i   0    1    2    3    4
+M = ['a', 'b', 'c', 'd', 'e']
+# -i -5   -4   -3   -2   -1
+
+for i in range(0, len(M)):  # функция len() - возвращает длину последовательности (кол-во элементов)
+    # print(i, end=' ')  # 0 1 2 3 4
+    print(M[i], end=' ')  # a b c d e
+print()
+
+s = 'abcde'
+for i in range(0, len(s)):
+    print(s[i], end=' ')  # a b c d e
+print()
+'''
+
+# через индексы можно менять элементы списков
+'''
+for i in range(0, len(M)):
+    M[i] = M[i] * i
+print(M)  # ['', 'b', 'cc', 'ddd', 'eeee']
+'''
+
+'''
+for x in M:
+    print(x, end=' ')  # a b c d e
+print()
+
+for x in M:
+    if x in 'ae':
+        print(x, end=' ')  #  a e 
+print()
+'''
+
+'''
+words = ['wfuh', 'wefe', 'rger', 'ergre']
+print(words[2].index('g'))  # 1
+'''
+
+
+# Цикл while: "цикл с условием: Выполняй действие, пока условие верно", "бесконечный цикл"
+
+# Перевод из 10-ной в 2-ную систему:
+'''
 x = int(input('x: '))
-if x % 2 == 0:
-    print('Четное ')
-else:
-    print('Нечетное ')
+n = 2
+M = []
+while x > 0:
+    M.append(x % n)  # добавление нового элемента в конец списка
+    x //= n
+M.reverse()  # метод для разворота элементов списка
+print(M)
+'''
 
-print()
-
-for x in range(10):
-    print(x, end=' ')
-print()
-
-print()
+# Бесконечные циклы
+'''
+i = 0
+while i < 10:
+    print(i)
+'''
+'''
+k = 1
+while True:
+    if k == 100000000:
+        break
+    k += 1
+    print(k)
 '''
 
 '''
-import math
-print(math.sqrt(16))    # взятие квадратного корня
+import random
+import time
 
-print(f'Возведем число 7 в степень 2: 7 ** 2 = {7**2} \n'
-      f'Взять квадратный корень от числа 16: 16 ** (1/2) = {16**(1/2)} \n'
-      f'Взять кубический корень от числа 27: 27 ** (1/3) = {27 ** (1/3)}')
+answers = ['Ошибка, введите пароль заново: ',
+           'Попробуйте повторить ввод пароля:',
+           'Повторите попытку снова:']
 
-import useful
-print(useful.Who_Is_Name())
-'''
-
-# Список библиотек, которые пригодятся на экзамене
-'''
-import math
-import string
-print(string.punctuation)  # !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
-
-import turtle  # для решения 6-го номера
-import ipaddress  # для решения новго 13-го номера 
-import itertools  # Может пригодиться в 8, 9, 24, 25 номерах
-import fnmatch  # для решения 25-го номера с масками
-import sys  # для решения 16-го номера 
-'''
-
-# Как пользоваться библиотеками
-'''
-import math
-print(math.sqrt(16))
-
-import math as m
-print(m.sqrt(16))
-
-from math import sqrt, factorial
-print(sqrt(16))
-
-from math import *
-print(sqrt(16))
-print(pow(7, 2))
-
-# Навести курсор и нажать сочетание клавиш ctrl + B
+pas = 'qwerty'
+count = 0
+password = input('Введите пароль для входа: ')
+while True:
+    if pas == password:
+        print('Welcome!')
+        break
+    count += 1
+    if count == 3:
+        print('Пройдите проверку на робота:')
+        a = random.randint(0, 100)
+        b = random.randint(0, 100)
+        x = int(input(f'{a} + {b} = '))
+        if x == a + b:
+            print('Проверка успешно пройдена!')
+            count = 0
+        else:
+            print('Повторите попытку через 20 секунд.')
+            time.sleep(20)
+    password = input(random.choice(answers))
 '''
 
 # endregion Урок: ******************************************************************
