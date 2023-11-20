b514908c34b583fd66f9c2a763cead7aaa28b577
Ilya Andrianov <ilandroxxy@gmail.com>
2023-11-21 01:07:46 +0700

    Занятия Понедельник `20.11.2023`:
    - #Никита2 Обсудили задачи с домашней работы и разобрали 12-ые номера.
    
    - #Диана Обсудили теорию множеств во всех красках.
    
    - #Евгений Разбирали задачи 12.2 и 14.2 с домашней работы, закрыли все вопросы.
    
    - #Марк Разбирали 25-е номера с делителями.
    
    - #ВасилийRemire Разобрали задачи 24-го номера с цифрами и поиском кол-ва этих букв.

--- ilandroxy_Bot/lessons/VasiliyRemire.py
+++ ilandroxy_Bot/lessons/VasiliyRemire.py
@@ -1,83 +1,85 @@
 
 # region Домашка: ******************************************************************
 
+'''
+count = 0
+m = 0
+f = open('17.txt')
+l = [int(i) for i in f]
 
+max_dvy = 0
+for i in range(len(l)):
+    if l[i] % 100 == 25:
+        max_dvy = max(max_dvy, l[i])
+print(max_dvy)
+for i in range(len(l) - 2):
+    c = 0
+    s = [l[i], l[i+1], l[i+2]]
+    for x in s:
+        if 999 < abs(x) < 10000:
+            c += 1
+    if c <= 2 and sum(s) <= max_dvy:
+        m = max(m, sum(s))
+        count += 1
+print(count, m)
+
+
+l = [int(i) for i in open('17.txt')]
+count = 0
+maxi = 0
+max_dvy = max([x for x in l if x % 100 == 25])
+for i in range(len(l) - 2):
+    S = [l[i], l[i + 1], l[i + 2]]
+    if len([x for x in S if len(str(abs(x))) == 4]) <= 2:
+        if sum(S) <= max_dvy:
+            m = max(m, sum(S))
+            count += 1
+print(count, m)  # 6315 84523
+'''
 # endregion Домашка: ******************************************************************
 
 
 # region Урок: ******************************************************************
 
-# Тип 24 №37159
-# Текстовый файл состоит не более, чем из 10**7 строчных букв английского алфавита.
-# Найдите максимальную длину подстроки, в которой символы «a» и «d» не стоят рядом.
+# Тип 24 №58329
+# Текстовый файл состоит не более чем из 10**6 символов арабских цифр (0,1,...,9).
+# Определите максимальное количество идущих подряд цифр,
+# среди которых сумма двух идущих подряд чисел больше числа следующего за ними.
 '''
 s = open('24.txt').readline()
-s = s.replace('ad', ' ').replace('da', ' ')
-print(max([len(sub_s) for sub_s in s.split()]))
-'''
-# Ответ: 2250
-
-
-# Тип 24 №48445
-# Текстовый файл содержит только буквы A, C, D, F, O.
-# Определите максимальное количество идущих подряд групп символов вида
-# согласная + согласная + гласная.
-'''
-from itertools import product
-M = []
-for x in product('ACDFO', repeat=3):
-    if x[0] in 'CDF' and x[1] in 'CDF' and x[2] in 'AO':
-        s = ''.join(x)
-        M.append(s)
-
-s = open('24.txt').readline()
-for x in M:
-    s = s.replace(x, '*')
-for x in 'ACDFO':
-    s = s.replace(x, ' ')
-
-print([len(x) for x in s.split()])
-print(max([len(x) for x in s.split()]))
-'''
-# Ответ: 5
-
-
-# Тип 24 №33196
-# Текстовый файл содержит только заглавные буквы латинского алфавита (ABC…Z).
-# Определите символ, который чаще всего встречается в файле сразу после буквы A.
-'''
-s = open('24.txt').readline()
-M = []
-for i in range(len(s)-1):
-    if s[i] == 'A':
-        M.append(s[i+1])
-print(max(set(M), key=M.count))
-'''
-# Вариант 2
-'''
-for x in set(M):
-    print(x, M.count(x))
-'''
-# Ответ: G
-
-
-# Тип 24 №29672
-# Текстовый файл содержит строки различной длины.
-# Общий объём файла не превышает 1 Мбайт.
-# Строки содержат только заглавные буквы латинского алфавита (ABC…Z).
-# Определите количество строк, в которых буква E встречается чаще, чем буква A.
-'''
-count = 0
-for s in open('24.txt'):
-    if s.count('E') > s.count('A'):
+max_count = 0
+count = 2
+for i in range(len(s)-2):
+    if int(s[i]) + int(s[i+1]) > int(s[i+2]):
         count += 1
-print(count)
+        max_count = max(max_count, count)
+    else:
+        count = 2
+print(max_count)
 '''
-# Ответ: 467
+# Ответ: 33
 
+
+
+# Тип 24 №27699
+# Текстовый файл состоит не более чем из 106 символов L, D и R.
+# Определите максимальную длину цепочки вида LDRLDRLDR...
+# (составленной из фрагментов LDR, последний фрагмент может быть неполным).
+'''
+s = open('24.txt').readline()
+print(s)
+print(len('LDRLDRLDRLDRLDR'))
+'''
+'''
+s = open('24.txt').readline()
+s = s.replace('LDR', '***').replace('*LD', '*++').replace('*L', '*-')
+s = s.replace('L', ' ').replace('D', ' ').replace('R', ' ')
+print(max([i for i in s.split()]))
+
+'''
 # endregion Урок: ******************************************************************
 
 
-# Василий = [2.1, 5.1, 6.1, 8.1, 12.1, 14.1, 15.1, 16.1, 17.1, 23.1]
+# Василий = [2.1, 5.1, 6.1, 8.1, 12.1, 14.1, 15.1, 16.1, 17.1, 23.1, 24.1]
 # КЕГЭ  = []
 # на следующем уроке:
