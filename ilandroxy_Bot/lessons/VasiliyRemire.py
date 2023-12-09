1d0ef695d2e1bbaff1a36cecc4edfe15067be110
Ilya Andrianov <ilandroxxy@gmail.com>
2023-12-09 13:39:08 +0700

    Занятия Четверг 07.12.2023:
    - #Варя Разобрали вторую часть 5 номера, задачи с разными системами счисления.
    
    - #Марк Разобрали 7 и 11 номера.
    
    - #ВасилийRemire Прорешивали домашку по 24 номерам, домашка 24.2

--- ilandroxy_Bot/lessons/VasiliyRemire.py
+++ ilandroxy_Bot/lessons/VasiliyRemire.py
@@ -2,64 +2,34 @@
 # region Домашка: ******************************************************************
 
 '''
-count = 0
-m = 0
-f = open('17.txt')
-l = [int(i) for i in f]
+M1 = [22, 11, 13, 20]
+M2 = [22, 11, 13, 4]
+M3 = [22, 35]
 
-max_dvy = 0
-for i in range(len(l)):
-    if l[i] % 100 == 25:
-        max_dvy = max(max_dvy, l[i])
-print(max_dvy)
-for i in range(len(l) - 2):
-    c = 0
-    s = [l[i], l[i+1], l[i+2]]
-    for x in s:
-        if 999 < abs(x) < 10000:
-            c += 1
-    if c <= 2 and sum(s) <= max_dvy:
-        m = max(m, sum(s))
-        count += 1
-print(count, m)
+s1 = 'aaabccca'
+s2 = 'aaabcccb'
+print(max(s1, s2))
 
-
-l = [int(i) for i in open('17.txt')]
-count = 0
-maxi = 0
-max_dvy = max([x for x in l if x % 100 == 25])
-for i in range(len(l) - 2):
-    S = [l[i], l[i + 1], l[i + 2]]
-    if len([x for x in S if len(str(abs(x))) == 4]) <= 2:
-        if sum(S) <= max_dvy:
-            m = max(m, sum(S))
-            count += 1
-print(count, m)  # 6315 84523
+print(max(M1, M2, M3))
 '''
+
+# todo Проверить в чем косяк (правильный ответ 273, а у нас получается 259)
+
+s = open('24.txt').readline()
+s =s.replace('D', 'D ').replace('A', ' A')
+M = []
+for x in s.split():
+    if 'A' in x and 'D' in x:
+        print(x)
+        M.append(x)
+print(max([len(x) for x in M]))
+
 # endregion Домашка: ******************************************************************
 
 
 # region Урок: ******************************************************************
 
-# № 2499 (Уровень: Базовый)
-#
-# Текстовый файл состоит не более чем из 106 символов X, Y и Z.
-#
-# Сколько раз в файле встречается последовательность символов XXXX?
-'''
-s = open('24.txt').readline()
-print(s.count('XXXX'))
-s = s.replace('XXXX', '****')
-s = s.replace('X', ' ').replace('Y', ' ').replace('Z', ' ')
-print(len(s.split()))
-'''
 
-s = open('24.txt').readline()
-count = 0
-for i in range(0, len(s)-3):
-    if s[i:i+4] == 'XXXX':
-        count += 1
-print(count)
 # endregion Урок: ******************************************************************
 
 
