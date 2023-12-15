c6a49ce70f8f6878102a076c33f81beecc027984
Ilya Andrianov <ilandroxxy@gmail.com>
2023-12-16 00:41:22 +0700

    Занятия Пятница `15.12.2023`:
    - #Тимур Разбирали теории циклов for и while
    
    - #Артур Разбирали 8 номера
    
    - #Никита Разбирали 7, 11 номера для подготовки к кр.
    
    - #Вика Провели первый урок по теории типов данных и базовой арифметике.
    
    - #Варя Рассмотрели вопросы по домашней работе 8.3 и поговорили про функции def
    
    - #Анастасия Разбирали 12 номера для подготовки к кр.

--- ilandroxy_Bot/lessons/barbarinathebestofall.py
+++ ilandroxy_Bot/lessons/barbarinathebestofall.py
@@ -1,189 +1,88 @@
 
 # region Домашка: ******************************************************************
+'''
+from itertools import product
+num = 1
+for s in product(sorted('ФЕВРАЛЬ'), repeat=6):
+    slovo = ''.join(s)
+    if 'Е' not in slovo and 'А' not in slovo:
+        print(num)
+        exit()
+    num += 1
+'''
+# 19609 ВВВВВВ
 
+
+
+# КЕГЭ № 1933 (Уровень: Базовый) (А. Куканова)
+# Вероника составляет слова, меняя местами буквы в слове КЛАБХАУС
+# так, что любые две соседние буквы различны между собой.
+# Сколько слов, включая исходное, может составить Вероника?
+'''
+from itertools import permutations
+count = set()
+for s in permutations('КЛАБХАУС', 8):
+    slovo = ''.join(s)
+    if all(slovo[i] != slovo[i+1] for i in range(len(slovo)-1)):
+        count.add(slovo)
+print(len(count))
+'''
 
 # endregion Домашка: ******************************************************************
 
 
 # region Урок: ********************************************************************
 
-# Тип 8 №9645
-# Все 4-буквенные слова, составленные из букв В, Л, Т, У, записаны в алфавитном порядке и пронумерованы.
-#
-# Вот начало списка:
-# 1. ВВВВ
-# 2. ВВВЛ
-# 3. ВВВТ
-# 4. ВВВУ
-# Запишите слово, которое стоит под номером 98.
+# Мы можем создавать свои функции
+
+# Создадим функцию поиска НОД для двух чисел
 '''
-s = sorted('ВЛТУ')
-num = 1
-for a in s:
-    for b in s:
-        for c in s:
-            for d in s:
-                slovo = a + b + c + d
-                if num == 98:
-                    print(num, slovo)
-                num += 1
-
-# Вариант 2
-s = sorted('ВЛТУ')
-words = [0]
-for a in s:
-    for b in s:
-        for c in s:
-            for d in s:
-                slovo = a + b + c + d
-                words.append(slovo)
-print(words[98])
+import math
+print(math.gcd(12, 24))
 
 
-# Вариант 3
-from itertools import product
-words = [0]
-for s in product(sorted('ВЛТУ'), repeat=4):
-    slovo = ''.join(s)
-    words.append(slovo)
-print(words[98])
+def NOD(x: int, y: int):
+    """
+    Функция поиска Наибольшего общего делителя чисел
+    """
+    nod = 1
+    for j in range(1, min(x, y)+1):
+        if x % j == 0 and y % j == 0:
+            nod = j
+    return nod
+
+
+print(NOD(12, 24))  # 12
+print(NOD.__doc__)
+print(help(NOD))
+# NOD(x: int, y: int)
+#     Функция поиска Наибольшего общего делителя чисел
+# None
 '''
-# Ответ: ЛТВЛ
 
-
-
-# Тип 8 №3206
-# Все 5-буквенные слова, составленные из букв А, К, Р, У, записаны в алфавитном порядке.
-# Вот начало списка:
-# 1. ААААА
-# 2. ААААК
-# 3. ААААР
-# 4. ААААУ
-# 5. АААКА
-
-# Укажите номер первого слова, которое начинается с буквы К.
+# Создадим функцию поиска простого числа
 '''
-from itertools import product
-num = 1
-for s in product(sorted('АКРУ'), repeat=5):
-    slovo = ''.join(s)
-    if slovo[0] == 'К':
-        print(num)
-        break
-    num += 1
-'''
-# Ответ: 257
-
-
-# Тип 8 №59744
-# Евгений составляет 6-буквенные слова из букв М, У, Ж, Ч, И, Н, А.
-# Каждая из букв может встречаться в слове ровно один раз, причём первой буквой не может быть Ч,
-# буква Ж должна встречаться не менее 1 раза и номер слова должен быть нечётный.
-#
-# Сколько различных слов может составить Евгений?
-'''
-s = 'МУЖЧИНА'
-count = 0
-for a in s:
-    for b in s:
-        for c in s:
-            for d in s:
-                for e in s:
-                    for f in s:
-                        slovo = a + b + c + d + e + f
-                        if len(slovo) == len(set(slovo)):  # Каждая из букв может встречаться в слове ровно один раз
-                            if a != 'Ч' and slovo.count('Ж') >= 1:
-                                count += 1
-print(count / 2)
+# 2 3 5 7 11 13 17 19 ... - простые числа
+# 4 6 8 10 12 14 15 16 ... - составные числа 
+# 1 2 3 4 5 6 7 8 9 10 11 ... inf - натуральные числа 
+# -inf,...,-1, 0, 1, ..., +inf - целые числа 
 '''
 '''
-from itertools import permutations
-count = 0
-for s in permutations('МУЖЧИНА', 6):
-    slovo = ''.join(s)
-    if slovo[0] != 'Ч' and slovo.count('Ж') >= 1:
-        count += 1
-print(count / 2)
+def Prime(x):
+    for j in range(2, x):
+        if x % j == 0:
+            return False
+    return True
+
+
+for x in range(1, 1000):
+    if Prime(x):
+        print(x)
 '''
-# Ответ: 1860
 
-# from itertools import permutations
-# for s in permutations('123'):
-#     print(s)
-
-
-# Тип 8 №27009
-# Николай составляет 4-буквенные коды из букв Н, И, К, О, Л, А, Й.
-# Каждую букву можно использовать любое количество раз, при этом код не может начинаться с буквы Й
-# и должен содержать хотя бы одну гласную.
-# Сколько различных кодов может составить Николай?
-'''
-from itertools import product
-count = 0
-for s in product('НИКОЛАЙ', repeat=4):
-    slovo = ''.join(s)
-    if slovo[0] != 'Й' and any(a in slovo for a in 'ИОА'):
-        count += 1
-print(count)
-'''
-# Ответ: 1866
-
-# Тип 8 №7986
-# Сколько слов длины 5, начинающихся с согласной буквы и
-# заканчивающихся гласной буквой, можно составить из букв З, И, М, А?
-# Каждая буква может входить в слово несколько раз.
-# Слова не обязательно должны быть осмысленными словами русского языка.
-'''
-from itertools import product
-count = 0
-for s in product('ЗИМА', repeat=5):
-    slovo = ''.join(s)
-    if slovo[0] in 'ЗМ':
-        if slovo[-1] in 'ИА':
-            count += 1
-print(count)
-'''
-# Ответ: 256
-
-
-# Тип 8 №46966
-# Светлана составляет коды из букв слова РОСОМАХА. Код должен состоять из 8 букв,
-# и каждая буква в нём должна встречаться столько же раз, сколько в заданном слове.
-# Кроме того, в коде не должны стоять рядом две гласные и две согласные буквы.
-# Сколько кодов может составить Светлана?
-'''
-s = 'РОСОМАХА'
-s1 = 'ОА'
-s2 = 'РСМХ'
-count = set()
-
-for a in s1:
-    for b in s2:
-        for c in s1:
-            for d in s2:
-                for e in s1:
-                    for f in s2:
-                        for g in s1:
-                            for h in s2:
-                                slovo = a + b + c + d + e + f + g + h
-                                if len(set(slovo)) == 6 and slovo.count('О') == 2 and s.count('А') == 2:
-                                    count.add(slovo)
-                                   
-print(len(count) * 2)
-'''
-# Ответ: 288
-
-
-
-'''
-from itertools import permutations
-count = 0
-for s in permutations('РОСОМАХА'):
-    slovo = ''.join(s)
-'''
 # endregion Урок: ******************************************************************
 
 
-# Варя = [2.1, 5.1, 6.1, 12.1, 14.1]
+# Варя = [2.1, 5.1, 6.1, 8.1, 12.1, 14.1]
 # КЕГЭ  = []
-# на следующем уроке:
+# на следующем уроке: 16, 23, 15
