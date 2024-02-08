3483a0012ee6c0b1c0530b432c2bce9198d09145
Ilya Andrianov <ilandroxxy@gmail.com>
2024-02-09 01:41:03 +0700

    Занятия Четверг `08.02.2024`:
    - #Максим Разобрали домашку 15.3 и успели разобрать базовые 9 номера. На следующем уроке добить 9 номера с температурами и разобрать 25 номера.
    
    - #Александра Прорешали вариант ОГЭ, на следующем уроке прореживать задачи ЕГЭ.
    
    - #Данил Разобрали 2 номер.
    
    - #Варя Разобрали 4 номер ЕГЭ и успели разобрать единицы измерения информации.
    
    - #Марк Прорешиваем КЕГЭ сложные 8 номера
    
    - #Лев Прорешали вопросы по домашке с списками. Разобрали базовые 14 номера.

--- ilandroxy_Bot/lessons/Danil.py
+++ ilandroxy_Bot/lessons/Danil.py
@@ -1,212 +1,76 @@
 # region Домашка: ******************************************************************
 
+# Дано натуральное число n. Напишите программу, которая определяет его максимальную и минимальную цифры.
+'''
+n = int(input())  # 534253465
+maxi = -9999999
+mini = 9999999
+while n > 0:
+    x = n % 10
+    if maxi < x:
+        maxi = x
+    mini = min(mini, x)
+    n //= 10
+print(maxi)
+print(mini)
+'''
+
+# Дано натуральное число. Напишите программу, которая вычисляет:
+#
+# сумму его цифр;
+# количество цифр в нем;
+# произведение его цифр;
+# среднее арифметическое его цифр;
+# его первую цифру;
+# сумму его первой и последней цифры.
+'''
+n = int(input())  # 324352
+summa = 0
+kol = 0
+pr = 1
+first = 0
+
+last = n % 10
+while n > 0:
+    x = n % 10
+    summa += x
+    kol += 1
+    pr *= x
+    first = x
+    n //= 10
+print(summa)
+print(kol)
+print(pr)
+print(summa / kol)
+print(first)
+print(first + last)
+'''
 # endregion Домашка: ******************************************************************
 
 
 # region Урок: ******************************************************************
 
 
-# Циклы в Пайтон
+print('x y z w')
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            for w in range(2):
+                # F = (z and y) or ((x <= z) == (y <= w))  # Тип 2 №15939 (z ∧ y) ∨ ((x → z ) ≡ (y → w))
+                F = ((z <= w) or (y == w)) and ((x or z) == y)  # Тип 2 #29109 ((z → w) ∨ (y ≡ w))∧((x∨z) ≡ y)
+                if F == True:
+                    print(x, y, z, w)
 
-# for - отвечает запросам: "повтори n раз", "пройди от А дол В"
-'''
-for i in range(10):  # range(START=0, STOP=10-1, STEP=1)
-    print(i, end=' ')  # 0 1 2 3 4 5 6 7 8 9
-print()
-
-for i in range(8):  # range(START=0, STOP=8-1, STEP=1)
-    print(i, end=' ')  # 0 1 2 3 4 5 6 7
-print()
-
-
-for i in range(2, 10):  # range(START=2, STOP=10-1, STEP=1)
-    print(i, end=' ')  # 2 3 4 5 6 7 8 9
-print()
-
-for i in range(2, 10, 2):  # range(START=2, STOP=10-1, STEP=2)
-    print(i, end=' ')  # 2 4 6 8
-print()
-
-for i in range(2, 10+1, 2):  # range(START=2, STOP=10+1-1, STEP=2)
-    print(i, end=' ')  # 2 4 6 8 10
-print()
-
-for i in range(10, 0, -1):  # range(START=10, STOP=0+1, STEP=-1)
-    print(i, end=' ')  # 10 9 8 7 6 5 4 3 2 1
-print()
-
-# i  0  1  2  3  4
-M = [1, 2, 3, 4, 5]
-for i in range(len(M)):  # len(M) - вернет длину (кол-во элементов) в списке М
-    # print(i, end=' ')  # 0 1 2 3 4
-    print(M[i], end=' ')  # 1 2 3 4 5
-print()
-
-
-# Назовём парой два идущих подряд элемента последовательности.
-# 12 23 34 45
-
-M = [1, 2, 3, 4, 5]
-for i in range(len(M)-1):
-    print(f'{M[i]}{M[i+1]}', end=' ')  # 12 23 34 45
-print()
-
-# Назовём тройкой три идущих подряд элемента последовательности.
-for i in range(len(M)-2):
-    print(f'{M[i]}{M[i+1]}{M[i+2]}', end=' ')  # 123 234 345
-print()
-
-# Можно пробегать последовательности напрямую
-M = [1, 2, 3, 4, 5]
-for x in M:
-    if x % 2 == 0:
-        print(x, end=' ')  # 2 4
-print()
-
-# Через range() мы можем изменять элементы списков
-M = [1, 2, 3, 4, 5]
-for i in range(len(M)-1):
-    M[i] = M[i] ** 2
-print(M)   # [1, 4, 9, 16, 5]
-'''
-
-
-# while - отвечает запросам: "выполняй действие, пока условие истинно", "бесконечные циклы"
-'''
-for i in range(2, 10+1, 2):  # range(START=2, STOP=10+1-1, STEP=2)
-    print(i, end=' ')  # 2 4 6 8 10
-print()
-
-
-i = 2
-while i <= 10:
-    print(i, end=' ')
-    i += 2
-'''
-
-# Перевод числа из 10-ной в n-ную систему счисления
-# 8_10 = 1000_2
-'''
-num = int(input())
-system = int(input())
-N = []
-while num > 0:
-    N.append(num % system)
-    num //= system
-N.reverse()
-print(N)
-'''
 
 '''
-num = int(input())
-system = int(input())
-s = ''
-while num > 0:
-    s += str(num % system)
-    num //= system
-s = s[::-1]
-print(s)  # 1000
+print('x y z w F')
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            for w in range(2):
+                F = (w <= (y == z)) and (y == (z <= x))  # Тип 2 №48450 (w → (y ≡ z)) ∧ (y ≡ (z → x))
+                print(x, y, z, w, int(F))
 '''
-
-'''
-alphabet = sorted('0123456789QWERTYUIOPASDFGHJKLZXCVBNM')
-num = int(input())  # 12345678976543213
-system = int(input())  # 20
-s = ''
-while num > 0:
-    s += alphabet[num % system]
-    num //= system
-s = s[::-1]
-print(s)  # 305CD1E037I0D
-'''
-'''
-print(int('305CD1E037I0D', 20))
-'''
-
-
-# Бесконечный цикл
-'''
-k = 0
-while True:
-    k += 1
-    print(k)
-'''
-
-'''
-alphabet = sorted('0123456789QWERTYUIOPASDFGHJKLZXCVBNM')
-while True:
-    case = input('case 1: Перевод из 10-ой в n-ую систему счисления \n'
-                 'case 2: Перевод из n-ой в 10-ую систему счисления \n'
-                 'case 3: Перевод из n-ой в k-ую систему счисления \n'
-                 'case 0: exit\n')
-
-    if case == '1':
-        num = int(input('Введите 10-е число: '))
-        system = int(input('Введите n-ую систему счисления: '))  # 20
-        ex_num = num
-        s = ''
-        while num > 0:
-            s += alphabet[num % system]
-            num //= system
-        s = s[::-1]
-        print(f'Число {ex_num} при переводе в {system}-ую систему счисления дает результат: {s} \n\n')
-
-    elif case == '2':
-        system = int(input('Введите n-ую систему счисления: '))
-        s = input(f'Введите число в {system}-ой системе счисления: ')
-        num = int(s, system)
-        print(f'При переводе числа {s} из {system}-ой системы счисления, получили: {num} \n\n')
-
-    elif case == '3':
-        system_n = int(input('Введите n-ую систему счисления: '))
-        s = input(f'Введите число в {system_n}-ой системе счисления: ')
-        system_k = int(input('Введите k-ую систему счисления: '))
-        num = int(s, system_n)
-        ex_num = num
-        s = ''
-        while num > 0:
-            s += alphabet[num % system_k]
-            num //= system_k
-        s = s[::-1]
-        print(f'Число {ex_num} при переводе из {system_n}-ой в {system_k}-оую системы получили: {s} \n\n')
-
-    elif case == '0':
-        break
-'''
-
-
-# Тип 14 №7460
-# Сколько единиц содержится в двоичной записи значения выражения: 4**2014 + 2**2015 - 8?
-'''
-x = 4**2014 + 2**2015 - 8
-M = []
-while x > 0:
-    M.append(x % 2)
-    x //= 2
-M.reverse()
-print(M.count(1))  # 2013
-
-# Вариант 2
-print(bin(4**2014 + 2**2015 - 8)[2:].count('1'))  # .count() - возвращает кол-во вхождений 
-'''
-# Ответ: 2013
-
-
-# Тип 14 №27385
-# Значение выражения 343**5 + 343**4 + 49**6 - 7**13 - 21 записали в системе счисления с основанием 7.
-# Сколько различных цифр содержит эта запись?
-'''
-x = 343**5 + 343**4 + 49**6 - 7**13 - 21
-M = []
-while x > 0:
-    M.append(x % 7)
-    x //= 7
-M.reverse()
-print(set(M))  # {0, 1, 4, 6}
-print(len(set(M)))  # 4
-'''
-# Ответ: 4
-
 # endregion Урок: ******************************************************************
 
 # Данил = []
--- ilandroxy_Bot/lessons/mant1ssss.py
+++ ilandroxy_Bot/lessons/mant1ssss.py
@@ -4,29 +4,73 @@
 
 
 # region Урок: ******************************************************************
+
+# КЕГЭ 13094
 '''
-def f(N , a):
-    s = ''
-    while N > 0:
-        s += str(N % a)
-        N //= a
-    s = s[::-1]
-    return s
-
-
-for N in range(1, 1000):
-    H = f(N, 4)
-    if len(H) % 2 == 0:
-        H = H[:len(H)//2] + '0' + H[len(H)//2:]
-    R = int(H)
-    if R <= 180:
-        print(N)
+count = 0
+s = '12345678'
+from itertools import product
+for s in product(s, repeat = 9):
+    chislo = ''.join(s)
+    if all([chislo.count(x) <= 3 for x in chislo]):
+        """if all((int(chislo[i])% 2 == 0) != (int(chislo[i+1])% 2 == 0) for i in range(len(chislo) - 1)):"""
+        chislo = chislo.replace('4', '2').replace('6', '2').replace('8', '2')
+        chislo = chislo.replace('3', '1').replace('5', '1').replace('7', '1')
+        if '22' not in chislo and '11' not in chislo:
+            count += 1
+print(count)
 '''
 
+
+# КЕГЭ 11827
+'''
+count = 0
+s = '01234567'
+from itertools import product
+for s in product(s, repeat = 7):
+    chislo = ''.join(s)
+    if chislo[0] != '0':
+        if len([x for x in chislo if int(x) % 2 == 0]) == 2:
+            if all(p not in chislo for p in '17 71 37 73 57 75 77'.split()):
+                count += 1
+print(count)
+'''
+
+
+# КЕГЭ 11202
+'''
+from itertools import permutations
+alphabet = sorted('АССЕМБЛЕР')
+count = 0
+slova = []
+for s in permutations(alphabet):
+    slovo = ''.join(s)
+    if sum([i+1 for i in range(len(slovo)) if slovo[i] in 'АЕ']) == 9:
+        slova.append(s)
+print(len(set(slova)))
+
+'''
+
+
+# Номер КЕГЭ 9363
+'''
+from itertools import permutations
+alphabet = 'ХОЧУНАБЮДЖЕТ'
+count = 0
+slova = []
+for s in permutations(alphabet):
+    slovo = ''.join(s)
+    for x in 'ОУЮЕ':
+        slovo = slovo.replace(x, "A")
+    if 'ААААА' not in slovo:
+        slova.append(slovo)
+print(len(set(slova)))
+
+'''
 
 # endregion Урок: *******************************************************************
 
 
 # Марк = [1.1, 2.1, 3.1, 6.1, 4.1, 5.1, 7.1, 8.1, 9.1, 12.1, 14.1, 15.1, 16.1, 17.1, 22.1, 23.1, 24.1, 25.1]
-# КЕГЭ  = []
+# КЕГЭ  = [5.1, 8.1, 14.1]
 # на следующем уроке:
