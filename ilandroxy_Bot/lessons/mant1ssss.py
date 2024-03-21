01d70f5b4589632b0fba0de11a51250360250aa7
Ilya Andrianov <ilandroxxy@gmail.com>
2024-03-21 23:55:37 +0700

    Занятия Четверг `21.03.2024`:
    - #Олеся Разобрали 24 номера базового уровня сложности.
    
    - #Максим Прорешали домашку 24 номера и разобрали сложные задачки на одну строку.
    
    - #Александра Порешали задачки практики на циклы while и for
    
    - #Данил Повторили 15 номер на отрезки, разобрали дз. Разобрали 9 номер ЕГЭ.
    
    - #Варя Разобрали проверочные работы Excel и начали разбирать 5 номера КЕГЭ. На следующем уроке продолжаем 5 номера средних и сложных типов.
    
    - #Марк Плотно прорешивали практику 24 номера, почти все задачи. Отработали КЕГЭ 24 номер.
    
    - #Лев Разобрали 7 и 11 номера. На следующем уроке разобрать 7 номера на музыку.

--- ilandroxy_Bot/lessons/mant1ssss.py
+++ ilandroxy_Bot/lessons/mant1ssss.py
@@ -5,104 +5,32 @@
 
 # region Урок: ******************************************************************
 
-
-# Определите в прилагаемом файле максимальное количество идущих подряд символов
-# (длину непрерывной подпоследовательности), среди которых символ T встречается ровно 3 раз.
-'''
-s = 'xxxxxxxxxxxTxxxxxxxTxxTxxxxxTxxxxTxxxxxxTxxxxxxxTxxxxxxxxxxTxxxxxxx'
-# ['xxxxxxxxxxx', 'xxxxxxx', 'xx', 'xxxxx', 'xxxx', 'xxxxxx', 'xxxxxxx', 'xxxxxxxxxx', 'xxxxxxx']
-# xxxxxxxxxxxTxxxxxxxTxxTxxxxx 28
-# xxxxxxxTxxTxxxxxTxxxx 21
-# xxTxxxxxTxxxxTxxxxxx 20
-# xxxxxTxxxxTxxxxxxTxxxxxxx 25
-# xxxxTxxxxxxTxxxxxxxTxxxxxxxxxx 30
-# xxxxxxTxxxxxxxTxxxxxxxxxxTxxxxxxx 33
-
-s = s.split('T')
-maxi = 0
-for i in range(len(s)-3):
-    # r = s[i] + 'T' + s[i+1] + 'T' + s[i+2] + 'T' + s[i+3]
-    r = 'T'.join(s[i:i+4])
-    maxi = max(maxi, len(r))
-    print(r, len(r))
-print(maxi)
-'''
-
-# Тип 24 №60266
+# КЕГЭ № 12931 PRO100 ЕГЭ 26.01.24 (Уровень: Базовый)
+#
 # Текстовый файл состоит из символов T, U, V, W, X, Y и Z.
-# Определите в прилагаемом файле максимальное количество идущих подряд символов
-# (длину непрерывной подпоследовательности), среди которых символ T встречается ровно 100 раз.
+# В прилагаемом файле определите максимальное количество символов в непрерывной подпоследовательности,
+# состоящей из идущих подряд групп символов VWXYZ в указанном порядке.
+# При этом в начале и в конце искомой последовательности группа символов VWXYZ может быть неполной.
+#
+# Искомая подпоследовательность должна содержать не менее одной полной группы символов VWXYZ.
 '''
-s = open('24.txt').readline().split('T')
+s = open('24.txt').readline()
+count = 4
 maxi = 0
-for i in range(len(s)-100):
-    r = 'T'.join(s[i:i+101])
-    maxi = max(maxi, len(r))
+for i in range(len(s)-4):
+    if s[i:i+5] in ['VWXYZ', 'WXYZV', 'XYZVW', 'YZVWX', 'ZVWXY']:
+        count += 1
+        maxi = max(maxi, count)
+    else:
+        count = 4
 print(maxi)
 '''
-# Ответ: 133
-
-
-# Определите минимальную длину подстроки, содержащую ровно 3 символов T.
-# Для выполнения этого задания следует написать программу.
-'''
-s = 'xxxxxxxxxxxTxxxxxxxTxxTxxxxxTxxxxTxxxxxxTxxxxxxxTxxxxxxxxxxTxxxxxxx'
-# ['xxxxxxxxxxx', 'xxxxxxx', 'xx', 'xxxxx', 'xxxx', 'xxxxxx', 'xxxxxxx', 'xxxxxxxxxx', 'xxxxxxx']
-# TxxxxxxxxxxxTxxxxxxxT 21
-# TxxxxxxxTxxT 12
-# TxxTxxxxxT 10
-# TxxxxxTxxxxT 12
-# TxxxxTxxxxxxT 13
-# TxxxxxxTxxxxxxxT 16
-# TxxxxxxxTxxxxxxxxxxT 20
-# TxxxxxxxxxxTxxxxxxxT 20
-
-s = s.split('T')
-mini = 9999999
-for i in range(len(s)-1):
-    r = 'T' + 'T'.join(s[i:i+2]) + 'T'
-    print(r, len(r))
-    mini = min(mini, len(r))
-print(mini)
-'''
-
-
-# Тип 24 №59794
-# Текстовый файл состоит не более чем из 106 символов латинского алфавита.
-# Определите минимальную длину подстроки, содержащую ровно 110 символов "U".
-# Для выполнения этого задания следует написать программу.
-'''
-s = open('24.txt').readline().split('U')
-mini = 99999
-for i in range(len(s)-108):
-    r = 'U' + 'U'.join(s[i:i+109]) + 'U'
-    mini = min(mini, len(r))
-print(mini)
-'''
-# Ответ: 1765
-
-
-#
-# № 12476 PRO100 ЕГЭ 29.12.23 (Уровень: Сложный)
-# Текстовый файл состоит из символов P, R, O, E, G – зашифрованное письмо Деду Морозу.
-#
-# Определите в прилагаемом файле максимальное количество идущих подряд символов, среди которых комбинация
-# символов RO встречается ровно 21 раз, а комбинации символов ORO и ROR ни разу не встречаются.
-'''
-s = open('24.txt').readline().split('RO')
-maxi = 0
-for i in range(len(s)-21):
-    r = 'RO'.join(s[i:i+22])
-    if 'ORO' not in r and 'ROR' not in r:
-        maxi = max(maxi, len(r))
-print(maxi)
-'''
-
 # endregion Урок: *******************************************************************
 
 
 # region Разобрать: *************************************************************
 
+# todo разобрать КЕГЭ № 11636 (Уровень: Сложный)
 
 # todo разобрать Марк № 14100 (Уровень: Средний)
 # (П. Говоров) В файле содержится строка длиной не более 106 из букв A,B,C.
