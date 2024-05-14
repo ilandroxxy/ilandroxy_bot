1a997855d1a112b42ebee97fd9088ac01579860a
Ilya Andrianov <ilandroxxy@gmail.com>
2024-05-15 00:43:25 +0700

    Занятия Понедельник `13Май2024`:
    - #Дмитрий Повторили решение нового прототипа 22 номера и разобрали задачи под вопросом из вариант Шастин 6, номера 9, 13
    
    - #Никита2 Быстро пробежались по задачам: 1, 4, 7, 11 и рассмотрели одну задачку с новым прототипом 22 номера.
    
    - #Никита Прорешивали досрочный вариант ЕГЭ, номера 1-15
    
    - #Евгений Повторили 22  номер и начали решать Досрочный вариант ЕГЭ, номера: 14, 8
    
    - #Марк Решили несколько сложных задач с канала (комментинг) и новые прототипы 22 номера обсудили
    
    - #Василий Повторили 15 номера всех типов

--- ilandroxy_Bot/lessons/d1ch01.py
+++ ilandroxy_Bot/lessons/d1ch01.py
@@ -7,28 +7,44 @@
 # region Урок: ******************************************************************
 
 '''
-from itertools import product
-cnt = 0
-for s in product('01234567', repeat=5):
-    slovo = ''.join(s)
-    if slovo[0] != '0':
-        if len(set(slovo)) == 3:
-            if any(p in slovo for p in '000 111 222 333 444 555 666 777'.split()):
-                cnt += 1
-print(cnt)
+def my_prod(M: list):
+    result = 1
+    for x in M:
+        result *= x
+    return result
 
+import math
+num = 0
+summa = 0
+for s in open("9.txt"):
+    M = [int(x) for x in s.split()]
+    num += 1
+    # if all((M[i] % 2) != (M[i+1] % 2) for i in range(len(M)-1)):
+    if all((M[i] + M[i+1]) % 2 != 0 for i in range(len(M)-1)):
+        copied = [x for x in M if M.count(x) > 1]
+        not_copied = [x for x in M if M.count(x) == 1]
+        # if (3 * sum(not_copied)) >= math.prod(copied):
+        if (3 * sum(not_copied)) >= my_prod(copied):
+            summa += num
 
-from itertools import product
-cnt = 0
-for s in product('01234567', repeat=5):
-    slovo = ''.join(s)
-    if slovo[0] != '0':
-        if len(set(slovo)) == 3:
-            if any(slovo[i] == slovo[i+1] == slovo[i+2] for i in range(len(s)-2)):
-                cnt += 1
-print(cnt)
+print(summa)
+'''
+'''
+alphabet = sorted('0123456789QWERTYUIOPASDFGHJKLZXCVBNM')
+for i in range(len(alphabet)):
+    print(i, alphabet[i])
+
+# U - 30
+for p in range(31, 37):
+    pass
 '''
 
+'''
+# 00000000, 1000000, 11000000, 11100000, 11110000, 11111000, 11111100, 11111110, 11111111
+s = '00000000 10000000 11000000 11100000 11110000 11111000 11111100 11111110 11111111'
+for x in s.split():
+    print(int(x, 2), end=' ')  # 0 128 192 224 240 248 252 254 255
+'''
 # endregion Урок: ******************************************************************
 
 
