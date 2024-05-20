659c4a9b1a7da08e87238060ba71c834a9c6591e
Ilya Andrianov <ilandroxxy@gmail.com>
2024-05-21 00:17:54 +0700

    Занятия Понедельник `20Май2024`:
    - #Дмитрий Повторяли 24 номера с практики Сетпика
    
    - #Никита Продолжили прорешивать 26 номера, на коржи/коробки и магазин.
    
    - #Евгений Разабрали задачи с Досрочного вариант ЕГЭ, номера: 14, 15, 23, 6, 12
    
    - #Марк Разобрали интересные задачи с варианта ЕГКР, номер: 7, 9, 14, 17, 24, 26
    
    - #Василий Повторяли 9 номера с практики Сетпика

--- ilandroxy_Bot/lessons/d1ch01.py
+++ ilandroxy_Bot/lessons/d1ch01.py
@@ -6,45 +6,66 @@
 
 # region Урок: ******************************************************************
 
+# КЕГЭ № 12254 ЕГКР 16.12.23 (Уровень: Базовый)
 '''
-def my_prod(M: list):
-    result = 1
-    for x in M:
-        result *= x
-    return result
-
-import math
-num = 0
-summa = 0
-for s in open("9.txt"):
-    M = [int(x) for x in s.split()]
-    num += 1
-    # if all((M[i] % 2) != (M[i+1] % 2) for i in range(len(M)-1)):
-    if all((M[i] + M[i+1]) % 2 != 0 for i in range(len(M)-1)):
-        copied = [x for x in M if M.count(x) > 1]
-        not_copied = [x for x in M if M.count(x) == 1]
-        # if (3 * sum(not_copied)) >= math.prod(copied):
-        if (3 * sum(not_copied)) >= my_prod(copied):
-            summa += num
-
-print(summa)
-'''
-'''
-alphabet = sorted('0123456789QWERTYUIOPASDFGHJKLZXCVBNM')
-for i in range(len(alphabet)):
-    print(i, alphabet[i])
-
-# U - 30
-for p in range(31, 37):
-    pass
+s = open('24.txt').readline()
+count = 2
+maxi = 0
+for i in range(len(s)-2):
+    if s[i:i+3] in ('RSQ', 'SQR', 'QRS'):
+        count += 1
+        maxi = max(maxi, count)
+    else:
+        count = 2
+print(maxi)
 '''
 
+# Не менее трех раз
+# oooXooooooXooooXoooooXoooooXoooooXooooXoooooXoooo
+# ['ooo', 'oooooo', 'oooo', 'ooooo', 'ooooo', 'ooooo', 'oooo', 'ooooo', 'oooo']
 '''
-# 00000000, 1000000, 11000000, 11100000, 11110000, 11111000, 11111100, 11111110, 11111111
-s = '00000000 10000000 11000000 11100000 11110000 11111000 11111100 11111110 11111111'
-for x in s.split():
-    print(int(x, 2), end=' ')  # 0 128 192 224 240 248 252 254 255
+s = open('24.txt').readline().split('X')
+mini = 10**9
+for i in range(len(s)-498):
+    r = 'X' + 'X'.join(s[i:i+499]) + 'X'
+    if 'Y' not in r:
+        mini = min(mini, len(r))
+print(mini)
 '''
+
+
+'''
+import string
+alphabet = string.ascii_uppercase
+print(alphabet)  # ABCDEFGHIJKLMNOPQRSTUVWXYZ
+
+
+s = open('24.txt').readline()
+for a in 'AEIOUY':
+    s = s.replace(a, 'A')
+
+for b in alphabet:
+    if b not in 'AEIOUY':
+        s = s.replace(b, 'B')
+
+while 'AA' in s or 'BB' in s:
+    s = s.replace('AA', 'A A').replace('BB', 'B B')
+
+print(max([len(x) for x in s.split()]))
+'''
+
+
+s = open('24.txt').readline()
+maxi = 0
+s = s.replace('C', 'C ').replace('D', 'D ').split()
+for i in range(len(s)-4):
+    r = ''.join(s[i:i+5])[:-1]
+    if r.count('D') == 2 and r.count('C') == 2:
+        maxi = max(maxi, len(r))
+print(maxi)
+
+
+
 # endregion Урок: ******************************************************************
 
 
